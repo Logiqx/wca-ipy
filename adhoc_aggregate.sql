6d7de1390ce3836f91612d678c59b5942b82fb90
Logiqx <software@mikeg.me.uk>
2019-01-31 13:24:16 +0000

    Use the Results and Competitions tables. Use age as it was on the first day.

--- adhoc_aggregate.sql
+++ adhoc_aggregate.sql
@@ -1,6 +1,6 @@
 /* 
     Script:   Aggregated Over 40's Rankings
-    Date:     2018-11-27
+    Date:     2019-01-31
     Author:   Michael George / 2015GEOR02
    
     Purpose:  Unofficial rankings for the Over 40's exist on GitHub but they are known to be incomplete - https://github.com/Logiqx/wca-ipy.
@@ -9,27 +9,24 @@
             
     Approach: The extract will not disclose any WCA IDs, regardless of whether they already appear in the unoffical rankings on GitHub.
               All consolidated results are modified using truncation / reduction of precision. This is typically to the nearest second.
-              
-    Notes:    This extract will potentially include personal records preceding the competitor being over 40 but this limitation will be deemed acceptable.
-              The age check is a little crude (checking YOB is before 1979) but when considered alongside the limitation above it isn't really significant.
 */
 
 
 /* 
-   Simple count of oldies from "RanksAverage" and "RanksSingle"
-
-   1) Use a simple year check to pick out the 40 year olds - i.e. year < 1979 (not really worried about Nov/Dec 1978 being wrong)
+   Simple counts of oldies present in "RanksAverage" and "RanksSingle"
 */
 
-SELECT eventId, COUNT(*)
+SELECT eventId, COUNT(*) AS num_persons,
+  SUM(TIMESTAMPDIFF(YEAR, DATE_FORMAT(CONCAT(p.year, "-", p.month, "-", p.day), "%Y-%m-%d"), CURDATE()) >= 40) AS num_oldies
 FROM RanksAverage r
-INNER JOIN Persons p ON p.id = r.personId AND p.year < 1979
+INNER JOIN Persons p ON p.id = r.personId
 GROUP BY eventId
 ORDER BY eventId;
 
-SELECT eventId, COUNT(*)
+SELECT eventId, COUNT(*) AS num_persons,
+  SUM(TIMESTAMPDIFF(YEAR, DATE_FORMAT(CONCAT(p.year, "-", p.month, "-", p.day), "%Y-%m-%d"), CURDATE()) >= 40) AS num_oldies
 FROM RanksSingle r
-INNER JOIN Persons p ON p.id = r.personId AND p.year < 1979
+INNER JOIN Persons p ON p.id = r.personId
 GROUP BY eventId
 ORDER BY eventId;
 
@@ -37,100 +34,56 @@ ORDER BY eventId;
    Extract AGGREGATED oldies from "RanksAverage"
    
    1) Output counts of oldies rather than WCA IDs
-   2) Leave "best" unchanged for known oldies, regardless of event - i.e. best
-   3) Truncate MBF to "points" only - i.e. FLOOR(best / 10000000) * 10000000
-   4) Truncate FMC "average" to the nearest move - i.e. FLOOR(best / 100) * 100
-   5) Truncate everything else to the nearest second - i.e. FLOOR(best / 100) * 100
-   6) Use a simple year check to pick out the 40 year olds - i.e. year < 1979 (not really worried about Nov/Dec 1978 being wrong)
+   2) Truncate results to the nearest second - i.e. FLOOR(best / 100)
+   3) Age check does not work properly on non-leap year birthdays
+   4) Optimisations pending
 */
 
-SELECT eventId,
-    FLOOR(best / 100) * 100 AS modifiedBest,
-    COUNT(*)
-FROM RanksAverage r
-INNER JOIN Persons p ON p.id = r.personId AND p.year < 1979
-WHERE eventId IN ('222', '333', '444', '555', '666', '777')
-GROUP BY eventId, modifiedBest
-ORDER BY eventId, modifiedBest;
-
-SELECT eventId,
-    FLOOR(best / 100) * 100 AS modifiedBest,
-    COUNT(*)
-FROM RanksAverage r
-INNER JOIN Persons p ON p.id = r.personId AND p.year < 1979
-WHERE eventId IN ('333oh', '333ft', '333bf', '444bf', '555bf')
-GROUP BY eventId, modifiedBest
-ORDER BY eventId, modifiedBest;
-
-SELECT eventId,
-    FLOOR(best / 100) * 100 AS modifiedBest,
-    COUNT(*)
-FROM RanksAverage r
-INNER JOIN Persons p ON p.id = r.personId AND p.year < 1979
-WHERE eventId IN ('pyram', 'skewb', 'clock', 'sq1', 'minx')
-GROUP BY eventId, modifiedBest
-ORDER BY eventId, modifiedBest;
-
-SELECT eventId,
-    (CASE
-        WHEN eventId IN ('333mbf', '333mbo') THEN FLOOR(best / 10000000) * 10000000
-        WHEN eventId IN ('333fm') THEN FLOOR(best / 100) * 100
-        ELSE FLOOR(best / 100) * 100
-    END) AS modifiedBest,
-    COUNT(*)
-FROM RanksAverage r
-INNER JOIN Persons p ON p.id = r.personId AND p.year < 1979
-WHERE eventId IN ('333mbf', '333mbo', '333fm')
-GROUP BY eventId, modifiedBest
-ORDER BY eventId, modifiedBest;
+SELECT eventId, FLOOR(best_average / 100) AS modified_average, COUNT(*) AS num_persons
+FROM
+(
+  SELECT eventId, personId, MIN(best) AS best_single, MIN(average) AS best_average
+  FROM
+  (
+    SELECT r.eventId, r.personId, r.best, r.average,
+        FLOOR(DATEDIFF(
+            DATE_FORMAT(CONCAT(c.year, "-", c.month, "-", c.day), "%Y-%m-%d"),
+            DATE_FORMAT(CONCAT(p.year, "-", p.month, "-", p.day), "%Y-%m-%d")) / 365.25) AS age_at_comp
+    FROM Results AS r
+    INNER JOIN Competitions AS c ON r.competitionId = c.id
+    INNER JOIN Persons AS p ON r.personId = p.id AND p.year > 0
+  ) tmp_results
+  WHERE average > 0 AND age_at_comp >= 40
+  GROUP BY eventId, personId
+) tmp_persons
+GROUP BY eventId, modified_average
+ORDER BY eventId, modified_average;
 
 /* 
    Extract AGGREGATED oldies from "RanksSingle"
    
    1) Output counts of oldies rather than WCA IDs
-   2) Leave "best" unchanged for known oldies, regardless of event - i.e. best
-   3) Truncate MBF to "points" only - i.e. FLOOR(best / 10000000) * 10000000
-   4) Leave FMC "single" as a move count - i.e. best
-   5) Truncate everything else to the nearest second - i.e. FLOOR(best / 100) * 100
-   6) Use a simple year check to pick out the 40 year olds - i.e. year < 1979 (not really worried about Nov/Dec 1978 being wrong)
+   2) Truncate results to the nearest second - i.e. FLOOR(best / 100)
+   3) Age check does not work properly on non-leap year birthdays
+   4) Optimisations pending
 */
 
-SELECT eventId,
-    FLOOR(best / 100) * 100 AS modifiedBest,
-    COUNT(*)
-FROM RanksSingle r
-INNER JOIN Persons p ON p.id = r.personId AND p.year < 1979
-WHERE eventId IN ('222', '333', '444', '555', '666', '777')
-GROUP BY eventId, modifiedBest
-ORDER BY eventId, modifiedBest;
-
-SELECT eventId,
-    FLOOR(best / 100) * 100 AS modifiedBest,
-    COUNT(*)
-FROM RanksSingle r
-INNER JOIN Persons p ON p.id = r.personId AND p.year < 1979
-WHERE eventId IN ('333oh', '333ft', '333bf', '444bf', '555bf')
-GROUP BY eventId, modifiedBest
-ORDER BY eventId, modifiedBest;
-
-SELECT eventId,
-    FLOOR(best / 100) * 100 AS modifiedBest,
-    COUNT(*)
-FROM RanksSingle r
-INNER JOIN Persons p ON p.id = r.personId AND p.year < 1979
-WHERE eventId IN ('pyram', 'skewb', 'clock', 'sq1', 'minx')
-GROUP BY eventId, modifiedBest
-ORDER BY eventId, modifiedBest;
-
-SELECT eventId,
-    (CASE
-        WHEN eventId IN ('333mbf', '333mbo') THEN FLOOR(best / 10000000) * 10000000
-        WHEN eventId IN ('333fm') THEN best
-        ELSE FLOOR(best / 100) * 100
-    END) AS modifiedBest,
-    COUNT(*)
-FROM RanksSingle r
-INNER JOIN Persons p ON p.id = r.personId AND p.year < 1979
-WHERE eventId IN ('333mbf', '333mbo', '333fm')
-GROUP BY eventId, modifiedBest
-ORDER BY eventId, modifiedBest;
+SELECT eventId, FLOOR(best_average / 100) AS modified_average, COUNT(*) AS num_persons
+FROM
+(
+  SELECT eventId, personId, MIN(best) AS best_single, MIN(average) AS best_average
+  FROM
+  (
+    SELECT r.eventId, r.personId, r.best, r.average,
+        FLOOR(DATEDIFF(
+            DATE_FORMAT(CONCAT(c.year, "-", c.month, "-", c.day), "%Y-%m-%d"),
+            DATE_FORMAT(CONCAT(p.year, "-", p.month, "-", p.day), "%Y-%m-%d")) / 365.25) AS age_at_comp
+    FROM Results AS r
+    INNER JOIN Competitions AS c ON r.competitionId = c.id
+    INNER JOIN Persons AS p ON r.personId = p.id AND p.year > 0
+  ) tmp_results
+  WHERE average > 0 AND age_at_comp >= 40
+  GROUP BY eventId, personId
+) tmp_persons
+GROUP BY eventId, modified_average
+ORDER BY eventId, modified_average;
