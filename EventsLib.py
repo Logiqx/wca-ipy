53f3b69ea9be62f1854489dae05774e0b6b474b2
Logiqx <software@mikeg.me.uk>
2019-02-13 22:08:24 +0000

    Decrease the number of groups for OH

--- EventsLib.py
+++ EventsLib.py
@@ -9,7 +9,7 @@ events = \
     ('777', '7x7x7 Cube', '60', 'time', 540, 720, 900),
     ('333bf', '3x3x3 Blindfolded', '70', 'time', 240, 300, 360),
     ('333fm', '3x3x3 Fewest Moves', '80', 'number', 60, 120, 180),
-    ('333oh', '3x3x3 One-Handed', '90', 'time', 180, 300, 360),
+    ('333oh', '3x3x3 One-Handed', '90', 'time', 120, 240, 300),
     ('333ft', '3x3x3 With Feet', '100', 'time', 180, 300, 360),
     ('clock', 'Clock', '110', 'time', 60, 120, 180),
     ('minx', 'Megaminx', '120', 'time', 240, 300, 360),
