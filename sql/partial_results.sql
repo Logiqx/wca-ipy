b7f6d1e1f55a5f3a09ef4566618b42c237e4274e
Logiqx <software@mikeg.me.uk>
2019-02-13 14:55:29 +0000

    Fix DOB additions

--- sql/partial_results.sql
+++ sql/partial_results.sql
@@ -155,8 +155,8 @@ ALTER TABLE Persons
 ADD COLUMN
 (
     `year` SMALLINT(5),
-    `year` SMALLINT(5),
     `month` SMALLINT(5),
+    `day` SMALLINT(5),
     `username` varchar(30) CHARACTER SET latin1 NULL
 );
 
