b67d1484e3fc9eedf7478dd24c04359fcca109ac
Logiqx <software@mikeg.me.uk>
2019-04-14 20:34:55 +0100

    Use latest persons table for delta processing

--- sql/extract_senior_deltas.sql
+++ sql/extract_senior_deltas.sql
@@ -32,7 +32,7 @@ FROM
         DATE_FORMAT(CONCAT(c.year, "-", c.month, "-", c.day), "%Y-%m-%d")) AS age_at_comp
     FROM wca_20190130.Results AS r
     INNER JOIN wca_20190130.Competitions AS c ON r.competitionId = c.id
-    INNER JOIN wca_20190130.Persons AS p ON r.personId = p.id AND p.subid = 1 AND p.year > 0 AND p.year <= YEAR(CURDATE()) - 40
+    INNER JOIN wca.Persons AS p ON r.personId = p.id AND p.subid = 1 AND p.year > 0 AND p.year <= YEAR(CURDATE()) - 40
     WHERE average > 0
     HAVING age_at_comp >= 40
   ) AS tmp_results
