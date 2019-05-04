05ffbe5ae4869a91fde0b189ae60b44fc1ce68cc
Logiqx <software@mikeg.me.uk>
2019-05-04 19:23:24 +0100

    No need for relative path of ipynb

--- bin/download_database.sh
+++ bin/download_database.sh
@@ -1,1 +1,1 @@
-docker exec wca_notebook_1 sh -c "cd work/wca-ipy/python; jupyter nbconvert --ExecutePreprocessor.timeout=300 --to notebook --execute --inplace ./Download_Database.ipynb"
+docker exec wca_notebook_1 sh -c "cd work/wca-ipy/python; jupyter nbconvert --ExecutePreprocessor.timeout=300 --to notebook --execute --inplace Download_Database.ipynb"
--- bin/refresh_rankings.sh
+++ bin/refresh_rankings.sh
@@ -1,4 +1,4 @@
-docker exec wca_notebook_1 sh -c "cd work/wca-ipy/python; jupyter nbconvert --to notebook --execute --inplace ./Format_Extracts.ipynb"
-docker exec wca_notebook_1 sh -c "cd work/wca-ipy/python; jupyter nbconvert --to notebook --execute --inplace ./Percentile_Rankings.ipynb"
-docker exec wca_notebook_1 sh -c "cd work/wca-ipy/python; jupyter nbconvert --to notebook --execute --inplace ./Senior_Rankings.ipynb"
-docker exec wca_notebook_1 sh -c "cd work/wca-ipy/python; jupyter nbconvert --to notebook --execute --inplace ./Partial_Rankings.ipynb"
+docker exec wca_notebook_1 sh -c "cd work/wca-ipy/python; jupyter nbconvert --to notebook --execute --inplace Format_Extracts.ipynb"
+docker exec wca_notebook_1 sh -c "cd work/wca-ipy/python; jupyter nbconvert --to notebook --execute --inplace Percentile_Rankings.ipynb"
+docker exec wca_notebook_1 sh -c "cd work/wca-ipy/python; jupyter nbconvert --to notebook --execute --inplace Senior_Rankings.ipynb"
+docker exec wca_notebook_1 sh -c "cd work/wca-ipy/python; jupyter nbconvert --to notebook --execute --inplace Partial_Rankings.ipynb"
