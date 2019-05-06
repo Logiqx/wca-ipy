c71abd9641124b432b534543c2299f77193e2e79
Logiqx <software@mikeg.me.uk>
2019-05-06 14:38:04 +0100

    Use container ID for docker exec

--- bin/download_database.sh
+++ bin/download_database.sh
@@ -1,1 +1,8 @@
-docker exec wca_notebook_1 sh -c "cd work/wca-ipy/python; jupyter nbconvert --ExecutePreprocessor.timeout=300 --to notebook --execute --inplace Download_Database.ipynb"
+# Determine the container ID in a way that works with Docker Compose and Docker Swarm
+CONTAINER=$(docker ps -q -f name=wca_notebook)
+
+# Location of Jupyter Notebooks
+PYTHON_DIR=work/wca-ipy/python
+
+# Run the download script
+docker exec $CONTAINER sh -c "cd $PYTHON_DIR; jupyter nbconvert --ExecutePreprocessor.timeout=300 --to notebook --execute --inplace Download_Database.ipynb"
--- bin/refresh_rankings.sh
+++ bin/refresh_rankings.sh
@@ -1,4 +1,11 @@
-docker exec wca_notebook_1 sh -c "cd work/wca-ipy/python; jupyter nbconvert --to notebook --execute --inplace Format_Extracts.ipynb"
-docker exec wca_notebook_1 sh -c "cd work/wca-ipy/python; jupyter nbconvert --to notebook --execute --inplace Percentile_Rankings.ipynb"
-docker exec wca_notebook_1 sh -c "cd work/wca-ipy/python; jupyter nbconvert --to notebook --execute --inplace Senior_Rankings.ipynb"
-docker exec wca_notebook_1 sh -c "cd work/wca-ipy/python; jupyter nbconvert --to notebook --execute --inplace Partial_Rankings.ipynb"
+# Determine the container ID in a way that works with Docker Compose and Docker Swarm
+CONTAINER=$(docker ps -q -f name=wca_notebook)
+
+# Location of Jupyter Notebooks
+PYTHON_DIR=work/wca-ipy/python
+
+# Run all of the scripts back-to-back
+docker exec $CONTAINER sh -c "cd $PYTHON_DIR; jupyter nbconvert --to notebook --execute --inplace Format_Extracts.ipynb"
+docker exec $CONTAINER sh -c "cd $PYTHON_DIR; jupyter nbconvert --to notebook --execute --inplace Partial_Rankings.ipynb"
+docker exec $CONTAINER sh -c "cd $PYTHON_DIR; jupyter nbconvert --to notebook --execute --inplace Senior_Rankings.ipynb"
+docker exec $CONTAINER sh -c "cd $PYTHON_DIR; jupyter nbconvert --to notebook --execute --inplace Percentile_Rankings.ipynb"
