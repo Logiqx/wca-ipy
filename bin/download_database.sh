5db3e7a32bd391240c3c227e079458d5fa2d408a
Logiqx <software@mikeg.me.uk>
2019-05-07 11:34:08 +0100

    Database download moved to the wca-db project

--- bin/download_database.sh
+++
@@ -1,8 +0,0 @@
-# Determine the container ID in a way that works with Docker Compose and Docker Swarm
-CONTAINER=$(docker ps -q -f name=wca_notebook)
-
-# Location of Jupyter Notebooks
-PYTHON_DIR=work/wca-ipy/python
-
-# Run the download script
-docker exec $CONTAINER sh -c "cd $PYTHON_DIR; jupyter nbconvert --ExecutePreprocessor.timeout=300 --to notebook --execute --inplace Download_Database.ipynb"
