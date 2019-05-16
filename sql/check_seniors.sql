4c235527a259f3041920ecb6da1c011aad35c709
Logiqx <software@mikeg.me.uk>
2019-05-16 09:32:28 +0100

    Add utility script

---
+++ sql/check_seniors.sql
@@ -0,0 +1,120 @@
+/*
+    Counts relating to the number of senior cubers in each country
+*/
+
+-- Countries with the most known oldies - UK + USA
+WITH cte1 AS
+(
+	SELECT p.countryId, COUNT(*) AS numPersons
+	FROM Seniors s
+	JOIN Persons p ON p.id = s.personId AND p.subid = 1
+	GROUP BY p.countryId
+)
+SELECT countryId, numPersons, ROUND(100.0 * numPersons / SUM(numPersons) OVER(), 2) AS pctOverall
+FROM cte1
+ORDER BY numPersons DESC;
+
+-- Countries with the greatest number of oldies as a percentage of the population - UK + Netherlands
+WITH cte1 AS
+(
+	SELECT p.countryId, COUNT(*) AS numPersons
+	FROM Seniors s
+	JOIN Persons p ON p.id = s.personId AND p.subid = 1
+	GROUP BY p.countryId
+),
+cte2 AS
+(
+	SELECT p.countryId, COUNT(*) AS numPersons
+	FROM Persons p
+    WHERE p.subid = 1
+	GROUP BY p.countryId
+)
+SELECT cte1.countryId, ROUND(100.0 * cte1.numPersons / cte2.numPersons, 2) AS pctOfPersons
+FROM cte1
+JOIN cte2 ON cte2.countryId = cte1.countryId
+ORDER BY pctOfPersons DESC;
+
+/*
+    Possible embassadors
+*/
+
+WITH cte AS
+(
+	SELECT p.id, p.name, p.countryId, s.username, s.dob, s.comment, c.year, COUNT(DISTINCT competitionId) AS numComps
+	FROM Seniors s
+	JOIN Persons p ON p.id = s.personId AND p.subid = 1
+	JOIN Results r ON r.personId = p.id
+	JOIN Competitions c ON c.id = r.competitionId
+	WHERE c.year >= 2017
+	GROUP BY p.id, p.name, p.countryId, s.dob, s.username, c.year, s.comment
+	HAVING COUNT(DISTINCT competitionId) >= 2
+	ORDER BY p.countryId, c.year DESC, COUNT(DISTINCT competitionId) DESC
+)
+SELECT c19.id, c19.name, c19.countryId, c19.username, -- c19.dob, c19.comment,
+	c19.numComps as numComps2019, IFNULL(c18.numComps, 0) AS numComps2018, IFNULL(c17.numComps, 0) AS numComps2017
+FROM cte AS c19
+LEFT JOIN cte c18 ON c18.id = c19.id and c18.year = 2018
+LEFT JOIN cte c17 ON c17.id = c19.id and c17.year = 2017
+WHERE c19.year = 2019;
+
+/*
+    Review comments
+*/
+
+-- Check for non-standard comments
+SELECT id, name, countryId, dob, username, comment
+FROM Seniors
+JOIN Persons ON id = personId AND subid = 1
+WHERE comment NOT LIKE 'Provided%' AND comment NOT LIKE 'Contacted%' AND comment NOT LIKE 'First%'
+AND comment NOT LIKE 'Found%' AND comment NOT LIKE 'Spotted%' AND comment NOT LIKE 'Speculative%';
+
+-- "Provided" indicates that the person (or someone on their behalf) pro-actively provided their information
+SELECT id, name, countryId, dob, username, comment
+FROM Seniors
+JOIN Persons ON id = personId AND subid = 1
+WHERE comment LIKE 'Provided%'
+AND NOT (comment LIKE '%qqwref%' OR comment LIKE '%Ron van Bruchem%' OR comment LIKE '%Andy Nicholls%')
+ORDER BY comment, countryId, personId;
+
+	-- Some people have provided multiple names
+	SELECT id, name, countryId, dob, username, comment
+	FROM Seniors
+	JOIN Persons ON id = personId AND subid = 1
+	WHERE comment LIKE 'Provided%'
+	AND (comment LIKE '%qqwref%' OR comment LIKE '%Ron van Bruchem%' OR comment LIKE '%Andy Nicholls%')
+	ORDER BY comment, countryId, personId;
+
+-- People contacted on Facebook
+SELECT id, name, countryId, dob, username, comment
+FROM Seniors
+JOIN Persons ON id = personId AND subid = 1
+WHERE comment LIKE 'Contacted%'
+ORDER BY comment, countryId, personId;
+
+-- People I added after meeting them at a competition
+SELECT id, name, countryId, dob, username, comment
+FROM Seniors
+JOIN Persons ON id = personId AND subid = 1
+WHERE comment LIKE 'First%'
+ORDER BY comment, countryId, personId;
+
+-- DOB / YOB that I found on the internet - Facebook, Wikipedia, Speedsolving, etc
+SELECT id, name, countryId, dob, username, comment
+FROM Seniors
+JOIN Persons ON id = personId AND subid = 1
+WHERE comment LIKE 'Found%'
+ORDER BY comment, countryId, personId;
+
+-- People that I spotted on the internet - Facebook, Speedsolving, etc
+SELECT id, name, countryId, dob, username, comment
+FROM Seniors
+JOIN Persons ON id = personId AND subid = 1
+WHERE comment LIKE 'Spotted%'
+ORDER BY comment, countryId, personId;
+
+-- Speculative additions - friends of friends, etc.
+SELECT id, name, countryId, dob, username, comment
+FROM Seniors
+JOIN Persons ON id = personId AND subid = 1
+WHERE comment LIKE 'Speculative%'
+ORDER BY countryId, comment, personId;
