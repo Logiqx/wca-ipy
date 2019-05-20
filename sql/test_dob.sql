f29bc5abbfd7a9647638badf7ef905b0461a73f2
Logiqx <software@mikeg.me.uk>
2019-05-20 09:19:16 +0100

    Improve DOB test

--- sql/test_dob.sql
+++ sql/test_dob.sql
@@ -6,13 +6,12 @@
     Purpose:  Test the impact of DOB - e.g. 1st Jan vs 31st Dec
 */
 
-SET @personId = '2005FARK01';
-SET @ageCategory = 40;
+SET @personId = '2015GEOR02';
+SET @ageCategory = 50;
 
-SET @month = '1953-07';
-SET @earliest = DATE_FORMAT(CONCAT(@month, "-01"), "%Y-%m-%d");
-SET @latest = DATE_ADD(DATE_ADD(@earliest, INTERVAL 1 MONTH), INTERVAL -1 DAY);
+SET @year = '1967'; SET @earliest = DATE_FORMAT(CONCAT(@year, "-01-01"), "%Y-%m-%d"); SET @latest = DATE_ADD(DATE_ADD(@earliest, INTERVAL 1 YEAR), INTERVAL -1 DAY);
 
+-- SET @month = '1968-10'; SET @earliest = DATE_FORMAT(CONCAT(@month, "-01"), "%Y-%m-%d"); SET @latest = DATE_ADD(DATE_ADD(@earliest, INTERVAL 1 MONTH), INTERVAL -1 DAY);
 
 SELECT e.id, e.name,
     earliest.best_single AS earliest_single, latest.best_single AS latest_single,
