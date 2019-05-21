182d204b7b9ff5e6d271a219fb4ee082d4e19fd2
Logiqx <software@mikeg.me.uk>
2019-05-21 07:38:29 +0100

    Split extracts from reports

---
+++ bin/format_extracts.sh
@@ -0,0 +1,8 @@
+# Determine the container ID in a way that works with Docker Compose and Docker Swarm
+CONTAINER=$(docker ps -q -f name=wca_notebook)
+
+# Location of Jupyter Notebooks
+PYTHON_DIR=work/wca-ipy/python
+
+# Run all of the scripts back-to-back
+docker exec $CONTAINER sh -c "cd $PYTHON_DIR; jupyter nbconvert --to notebook --execute --inplace Format_Extracts.ipynb"
--- bin/refresh_rankings.sh
+++ bin/refresh_rankings.sh
@@ -5,7 +5,6 @@ CONTAINER=$(docker ps -q -f name=wca_notebook)
 PYTHON_DIR=work/wca-ipy/python
 
 # Run all of the scripts back-to-back
-docker exec $CONTAINER sh -c "cd $PYTHON_DIR; jupyter nbconvert --to notebook --execute --inplace Format_Extracts.ipynb"
 docker exec $CONTAINER sh -c "cd $PYTHON_DIR; jupyter nbconvert --to notebook --execute --inplace Partial_Rankings.ipynb"
 docker exec $CONTAINER sh -c "cd $PYTHON_DIR; jupyter nbconvert --to notebook --execute --inplace Senior_Rankings.ipynb"
 docker exec $CONTAINER sh -c "cd $PYTHON_DIR; jupyter nbconvert --to notebook --execute --inplace Percentile_Rankings.ipynb"
