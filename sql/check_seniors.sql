0b00e9d1af4eb67c714ea2c0c392951d027f67bc
Logiqx <software@mikeg.me.uk>
2019-06-02 20:22:11 +0100

    Remove ORDER BY as it is in the view

--- sql/check_seniors.sql
+++ sql/check_seniors.sql
@@ -8,44 +8,37 @@
 
 -- List everyone
 SELECT 'Everyone' AS label, s.*
-FROM SeniorDetails AS s
-ORDER BY lastComp DESC, numComps DESC, yearsCompeting DESC;
+FROM SeniorDetails AS s;
 
 -- List the over 50s
 SELECT 'Over 50' AS label, s.*
 FROM SeniorDetails AS s
-WHERE ageLastComp >= 50
-ORDER BY lastComp DESC, numComps DESC, yearsCompeting DESC;
+WHERE ageLastComp >= 50;
 
 -- List the over 60s
 SELECT 'Over 60' AS label, s.*
 FROM SeniorDetails AS s
-WHERE ageLastComp >= 60
-ORDER BY lastComp DESC, numComps DESC, yearsCompeting DESC;
+WHERE ageLastComp >= 60;
 
 -- List the over 70s
 SELECT 'Over 70' AS label, s.*
 FROM SeniorDetails AS s
-WHERE ageLastComp >= 70
-ORDER BY lastComp DESC, numComps DESC, yearsCompeting DESC;
+WHERE ageLastComp >= 70;
 
 -- List the over 80s
 SELECT 'Over 80' AS label, s.*
 FROM SeniorDetails AS s
-WHERE ageLastComp >= 80
-ORDER BY lastComp DESC, numComps DESC, yearsCompeting DESC;
+WHERE ageLastComp >= 80;
 
 -- List the over 90s
 SELECT 'Over 90' AS label, s.*
 FROM SeniorDetails AS s
-WHERE ageLastComp >= 90
-ORDER BY lastComp DESC, numComps DESC, yearsCompeting DESC;
+WHERE ageLastComp >= 90;
 
 -- List the delegates appearing in the senior rankings
 SELECT 'Delegate' AS label, delegate_status, s.*
 FROM SeniorDetails AS s
-JOIN wca_dev.users u ON u.wca_id = s.personId AND delegate_status IS NOT NULL
-ORDER BY lastComp DESC, numComps DESC, yearsCompeting DESC;
+JOIN wca_dev.users u ON u.wca_id = s.personId AND delegate_status IS NOT NULL;
 
 -- List the possible 'embassadors' for the senior rankings
 WITH SeniorYears AS
@@ -66,64 +59,54 @@ LEFT JOIN SeniorYears y1 ON y1.personId = s.personId and y1.year = YEAR(NOW()) -
 LEFT JOIN SeniorYears y2 ON y2.personId = s.personId and y2.year = YEAR(NOW()) - 2
 LEFT JOIN wca_dev.users u ON u.wca_id= s.personId
 HAVING numComps1 >= 6 AND numComps0 >= CEIL((MONTH(NOW()) - 1) / 2)
-ORDER BY countryId;
+ORDER BY countryId, name;
 
 -- Summarise the sources
 SELECT 'Source' AS label, sourceType, COUNT(*) AS numSeniors
 FROM SeniorDetails s
-GROUP BY sourceType
-ORDER BY numSeniors DESC;
+GROUP BY sourceType;
 
 -- Summarise the comment types
 SELECT 'Comment' AS label, LEFT(comment, LOCATE(' ', comment) - 1) AS commentType, COUNT(*) AS numSeniors
 FROM SeniorDetails s
-GROUP BY commentType
-ORDER BY numSeniors DESC;
+GROUP BY commentType;
 
 -- List people who pro-actively provided their information (or someone did so on their behalf)
 SELECT LEFT(comment, LOCATE(' ', comment) - 1) AS label, s.*
 FROM SeniorDetails AS s
-WHERE comment LIKE 'Provided%'
-ORDER BY lastComp DESC, numComps DESC, yearsCompeting DESC;
+WHERE comment LIKE 'Provided%';
 
 -- List people contacted via Facebook
 SELECT LEFT(comment, LOCATE(' ', comment) - 1) AS label, s.*
 FROM SeniorDetails AS s
-WHERE comment LIKE 'Contacted%'
-ORDER BY lastComp DESC, numComps DESC, yearsCompeting DESC;
+WHERE comment LIKE 'Contacted%';
 
 -- List people where DOB / YOB was found on the internet - Facebook, Wikipedia, Speedsolving, etc
 SELECT LEFT(comment, LOCATE(' ', comment) - 1) AS label, s.*
 FROM SeniorDetails AS s
-WHERE comment LIKE 'Found%'
-ORDER BY lastComp DESC, numComps DESC, yearsCompeting DESC;
+WHERE comment LIKE 'Found%';
 
 -- List people spotted on the internet - Facebook, WCA, Speedsolving, etc
 SELECT LEFT(comment, LOCATE(' ', comment) - 1) AS label, s.*
 FROM SeniorDetails AS s
-WHERE comment LIKE 'Spotted%'
-ORDER BY lastComp DESC, numComps DESC, yearsCompeting DESC;
+WHERE comment LIKE 'Spotted%';
 
 -- List speculative additions - friends of friends, etc.
 SELECT LEFT(comment, LOCATE(' ', comment) - 1) AS label, s.*
 FROM SeniorDetails AS s
-WHERE comment LIKE 'Speculative%'
-ORDER BY lastComp DESC, numComps DESC, yearsCompeting DESC;
+WHERE comment LIKE 'Speculative%';
 
 -- Summarise the accuracy of DOB information
 SELECT 'Accuracy' AS label, accuracyType, COUNT(*) AS numSeniors
 FROM SeniorDetails s
-GROUP BY accuracyType
-ORDER BY numSeniors DESC;
+GROUP BY accuracyType;
 
--- Imprecise DOBs - Y = year, X = approximated year, F = faked, U = Undefined
+-- Imprecise DOBs
 SELECT 'Imprecise DOB' AS label, s.*
 FROM SeniorDetails AS s
-WHERE accuracyId NOT IN ('D', 'M', 'S') -- D = day, M = month, S = synthetic
-ORDER BY accuracyType, lastComp DESC, numComps DESC, yearsCompeting DESC;
+WHERE accuracyId NOT IN ('D', 'M', 'S');
 
 -- Speedsolving.com users to be discovered
 SELECT 'Speedsolving.com' AS label, s.*
 FROM SeniorDetails AS s
-WHERE usernum = 0
-ORDER BY accuracyType, lastComp DESC, numComps DESC, yearsCompeting DESC;
+WHERE usernum = 0;
