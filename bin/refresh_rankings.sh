5143a4af8be1b937290341756f9d2e6dc40f8795
Logiqx <software@mikeg.me.uk>
2019-06-03 08:40:08 +0100

    Add timers

--- bin/refresh_rankings.sh
+++ bin/refresh_rankings.sh
@@ -5,6 +5,8 @@ CONTAINER=$(docker ps -q -f name=wca_notebook)
 PYTHON_DIR=work/wca-ipy/python
 
 # Run all of the scripts back-to-back
-docker exec $CONTAINER sh -c "cd $PYTHON_DIR; jupyter nbconvert --to notebook --execute --inplace Partial_Rankings.ipynb"
-docker exec $CONTAINER sh -c "cd $PYTHON_DIR; jupyter nbconvert --to notebook --execute --inplace Senior_Rankings.ipynb"
-docker exec $CONTAINER sh -c "cd $PYTHON_DIR; jupyter nbconvert --to notebook --execute --inplace Percentile_Rankings.ipynb"
+time docker exec $CONTAINER sh -c "cd $PYTHON_DIR; jupyter nbconvert --to notebook --execute --inplace Partial_Rankings.ipynb"
+echo
+time docker exec $CONTAINER sh -c "cd $PYTHON_DIR; jupyter nbconvert --to notebook --execute --inplace Senior_Rankings.ipynb"
+echo
+time docker exec $CONTAINER sh -c "cd $PYTHON_DIR; jupyter nbconvert --to notebook --execute --inplace Percentile_Rankings.ipynb"
