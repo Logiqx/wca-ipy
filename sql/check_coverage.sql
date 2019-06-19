32b23b86b365e4a06e60d77024fbb7065b7ad6b7
Logiqx <software@mikeg.me.uk>
2019-06-19 17:43:51 +0100

    Re-factor SQL

--- sql/check_coverage.sql
+++ sql/check_coverage.sql
@@ -9,31 +9,29 @@
 -- Countries with the most known oldies - UK + USA
 WITH cte1 AS
 (
-	SELECT p.countryId, COUNT(*) AS numPersons
-	FROM wca_ipy.Seniors s
-	JOIN wca.Persons p ON p.id = s.personId AND p.subid = 1
-	GROUP BY p.countryId
+	SELECT countryId, COUNT(*) AS numSeniors
+	FROM wca_ipy.Seniors
+	GROUP BY countryId
 )
-SELECT countryId, numPersons, ROUND(100.0 * numPersons / SUM(numPersons) OVER(), 2) AS pctOverall
+SELECT countryId, numSeniors, ROUND(100.0 * numSeniors / SUM(numSeniors) OVER(), 2) AS pctOverall
 FROM cte1
-ORDER BY numPersons DESC;
+ORDER BY numSeniors DESC;
 
 -- Countries with the greatest number of oldies as a percentage of the population - UK + Netherlands
 WITH cte1 AS
 (
-	SELECT p.countryId, COUNT(*) AS numPersons
-	FROM wca_ipy.Seniors s
-	JOIN wca.Persons p ON p.id = s.personId AND p.subid = 1
-	GROUP BY p.countryId
+	SELECT countryId, COUNT(*) AS numSeniors
+	FROM wca_ipy.Seniors
+	GROUP BY countryId
 ),
 cte2 AS
 (
 	SELECT p.countryId, COUNT(*) AS numPersons
-	FROM wca.Persons p
+	FROM Persons p
     WHERE p.subid = 1
 	GROUP BY p.countryId
 )
-SELECT cte1.countryId, cte1.numPersons AS numSeniorsCountry, cte2.numPersons AS numPersonsCountry, ROUND(100.0 * cte1.numPersons / cte2.numPersons, 2) AS pctSeniors
+SELECT cte1.countryId, numSeniors, numPersons, ROUND(100.0 * numSeniors / numPersons, 2) AS pctSeniors
 FROM cte1
 JOIN cte2 ON cte2.countryId = cte1.countryId
 ORDER BY pctSeniors DESC;
