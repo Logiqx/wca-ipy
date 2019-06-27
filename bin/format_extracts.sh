dcfad9c800f2c1567415d096127e46e23d15d836
Logiqx <software@mikeg.me.uk>
2019-06-27 11:39:18 +0100

    Increase timeout for when machine is super-busy

--- bin/format_extracts.sh
+++ bin/format_extracts.sh
@@ -5,4 +5,4 @@ CONTAINER=$(docker ps -q -f name=wca_notebook)
 PYTHON_DIR=work/wca-ipy/python
 
 # Run all of the scripts back-to-back
-time docker exec $CONTAINER sh -c "cd $PYTHON_DIR; jupyter nbconvert --to notebook --execute --inplace Format_Extracts.ipynb"
+time docker exec $CONTAINER sh -c "cd $PYTHON_DIR; jupyter nbconvert --ExecutePreprocessor.timeout=120 --to notebook --execute --inplace Format_Extracts.ipynb"
