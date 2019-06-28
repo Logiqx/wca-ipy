493756b900bdfe12876d23f223e1b8b4f30dd0d3
Logiqx <software@mikeg.me.uk>
2019-06-28 09:22:27 +0100

    Fix collation

--- sql/load_seniors.sql
+++ sql/load_seniors.sql
@@ -10,14 +10,14 @@ DROP TABLE IF EXISTS wca_ipy.Seniors;
 
 CREATE TABLE wca_ipy.Seniors
 (
-     `personId` varchar(10) CHARACTER SET utf8 NOT NULL,
+     `personId` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
      `sourceId` char(1) NOT NULL DEFAULT 'U',
      `hidden` char(1) NOT NULL DEFAULT 'Y',
      `accuracyId` char(1) NOT NULL,
      `dob` date NOT NULL,
-     `username` varchar(30) CHARACTER SET utf8,
+     `username` varchar(30) COLLATE utf8mb4_unicode_ci,
      `usernum` mediumint NOT NULL DEFAULT 0,
-     `comment` text CHARACTER SET utf8 NOT NULL,
+     `comment` text COLLATE utf8mb4_unicode_ci NOT NULL,
      PRIMARY KEY (`personId`),
      FOREIGN KEY (`sourceId`) REFERENCES SeniorSources(`id`),
      FOREIGN KEY (`accuracyId`) REFERENCES SeniorAccuracies(`id`)
