f7a8dbd6c107883c92d75093b748bd73600bc6bf
Logiqx <software@mikeg.me.uk>
2019-07-06 18:35:29 +0100

    Re-factored aggregations

--- sql/extract_senior_aggs.sql
+++ sql/extract_senior_aggs.sql
@@ -6,20 +6,21 @@
     Purpose:  Private extract to facilitate the production of "Indicative Senior Rankings"
               https://logiqx.github.io/wca-ipy/Indicative_Rankings.html
             
-    Approach: All consolidated results are modified using truncation / reduction of precision:
-              1) Truncate MBF to "points" only - i.e. FLOOR(best / 10000000)
-              2) Leave FMC "single" as a move count - i.e. best
-              3) Truncate everything else to the nearest second - i.e. FLOOR(best / 100)
-
     Notes:    The extracts will not disclose any WCA IDs, regardless of whether they are already known
+              All consolidated results are modified using truncation / reduction of precision:
               These extracts will never be shared publicly
 */
 
-DROP TEMPORARY TABLE IF EXISTS senior_prs;
-CREATE TEMPORARY TABLE senior_prs AS
+/*
+   Create temporary table containing "senior bests" - one record per person, per event, per age category
+   This is essentially the query from extract_senior_results.sql
+*/
+
+-- First temporary table only contains actual results - "age category" based on "age at competition"
+DROP TEMPORARY TABLE IF EXISTS senior_bests_1;
+CREATE TEMPORARY TABLE senior_bests_1 AS
 SELECT eventId, personId, FLOOR(age_at_comp / 10) * 10 AS age_category,
-  MIN(CASE WHEN eventId IN ('333mbf', '333mbo') THEN FLOOR(best / 10000000) WHEN eventId IN ('333fm') THEN best ELSE FLOOR(best / 100) END) AS modified_single,
-  MIN(IF(average > 0, FLOOR(average / 100), NULL)) AS modified_average
+  MIN(best) AS best_single, MIN(IF(average > 0, average, NULL)) AS best_average
 FROM
 (
   SELECT r.eventId, r.personId, r.best, r.average,
@@ -34,66 +35,42 @@ FROM
 ) AS senior_results
 GROUP BY eventId, personId, age_category;
 
--- Combine multiple queries for different age categories
-SELECT eventId, modified_average, 40 AS age_category, COUNT(DISTINCT personId) AS num_persons
-FROM senior_prs
-WHERE age_category >= 40
-GROUP BY eventId, modified_average
-UNION ALL
-SELECT eventId, modified_average, 50 AS age_category, COUNT(DISTINCT personId) AS num_persons
-FROM senior_prs
-WHERE age_category >= 50
-GROUP BY eventId, modified_average
-UNION ALL
-SELECT eventId, modified_average, 60 AS age_category, COUNT(DISTINCT personId) AS num_persons
-FROM senior_prs
-WHERE age_category >= 60
-GROUP BY eventId, modified_average
-UNION ALL
-SELECT eventId, modified_average, 70 AS age_category, COUNT(DISTINCT personId) AS num_persons
-FROM senior_prs
-WHERE age_category >= 70
-GROUP BY eventId, modified_average
-UNION ALL
-SELECT eventId, modified_average, 80 AS age_category, COUNT(DISTINCT personId) AS num_persons
-FROM senior_prs
-WHERE age_category >= 80
-GROUP BY eventId, modified_average
-UNION ALL
-SELECT eventId, modified_average, 90 AS age_category, COUNT(DISTINCT personId) AS num_persons
-FROM senior_prs
-WHERE age_category >= 90
-GROUP BY eventId, modified_average
+-- Second temporary table also contains backdated results - "over 50" also counts as "over 40", etc
+DROP TEMPORARY TABLE IF EXISTS senior_bests_2;
+CREATE TEMPORARY TABLE senior_bests_2 AS
+SELECT eventId, personId, a.age_category, MIN(best_single) AS best_single, MIN(best_average) AS best_average
+FROM senior_bests_1 AS s
+JOIN (SELECT DISTINCT age_category FROM senior_bests_1) AS a ON a.age_category <= s.age_category
+GROUP BY eventId, personId, age_category;
+
+/*
+   Extract AGGREGATED senior results (averages)
+   1) Output counts of seniors rather than WCA IDs
+   2) Truncate everything to the nearest second - i.e. FLOOR(best / 100)
+*/
+
+SELECT eventId,
+  FLOOR(best_average / 100) AS modified_average,
+  age_category, COUNT(DISTINCT personId) AS num_persons
+FROM senior_bests_2
+WHERE best_average > 0
+GROUP BY eventId, modified_average, age_category
 ORDER BY eventId, modified_average, age_category;
 
--- Combine multiple queries for different age categories
-SELECT eventId, modified_single, 40 AS age_category, COUNT(DISTINCT personId) AS num_persons
-FROM senior_prs
-WHERE age_category >= 40
-GROUP BY eventId, modified_single
-UNION ALL
-SELECT eventId, modified_single, 50 AS age_category, COUNT(DISTINCT personId) AS num_persons
-FROM senior_prs
-WHERE age_category >= 50
-GROUP BY eventId, modified_single
-UNION ALL
-SELECT eventId, modified_single, 60 AS age_category, COUNT(DISTINCT personId) AS num_persons
-FROM senior_prs
-WHERE age_category >= 60
-GROUP BY eventId, modified_single
-UNION ALL
-SELECT eventId, modified_single, 70 AS age_category, COUNT(DISTINCT personId) AS num_persons
-FROM senior_prs
-WHERE age_category >= 70
-GROUP BY eventId, modified_single
-UNION ALL
-SELECT eventId, modified_single, 80 AS age_category, COUNT(DISTINCT personId) AS num_persons
-FROM senior_prs
-WHERE age_category >= 80
-GROUP BY eventId, modified_single
-UNION ALL
-SELECT eventId, modified_single, 90 AS age_category, COUNT(DISTINCT personId) AS num_persons
-FROM senior_prs
-WHERE age_category >= 90
-GROUP BY eventId, modified_single
+/*
+   Extract AGGREGATED senior results (singles)
+   1) Output counts of seniors rather than WCA IDs
+   2) Truncate MBF to "points" only - i.e. FLOOR(best / 10000000)
+   3) Leave FMC "single" as a move count - i.e. best
+   4) Truncate everything else to the nearest second - i.e. FLOOR(best / 100)
+*/
+
+SELECT eventId,
+  CASE WHEN eventId IN ('333mbf', '333mbo') THEN FLOOR(best_single / 10000000)
+    WHEN eventId IN ('333fm') THEN best_single
+    ELSE FLOOR(best_single / 100)
+  END AS modified_single,
+  age_category, COUNT(DISTINCT personId) AS num_persons
+FROM senior_bests_2
+GROUP BY eventId, modified_single, age_category
 ORDER BY eventId, modified_single, age_category;
