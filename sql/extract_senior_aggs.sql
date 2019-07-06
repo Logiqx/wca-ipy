607e68f0a2762a88b117eacadfc391e2bdfd9837
Logiqx <software@mikeg.me.uk>
2019-07-06 10:14:14 +0100

    Re-factored SQL

--- sql/extract_senior_aggs.sql
+++ sql/extract_senior_aggs.sql
@@ -1,79 +1,99 @@
 /* 
-    Script:   Aggregated Over 40s Rankings
-    Created:  2019-02-07
+    Script:   Extract Senior Results (Aggregated)
+    Created:  2019-07-06
     Author:   Michael George / 2015GEOR02
    
-    Purpose:  Provide a comprehensive view of the over 40s performances in competition
+    Purpose:  Private extract to facilitate the production of "Indicative Senior Rankings"
+              https://logiqx.github.io/wca-ipy/Indicative_Rankings.html
             
-    Approach: The extract will not disclose any WCA IDs, regardless of whether they are already known
-              All consolidated results are modified using truncation / reduction of precision
+    Approach: All consolidated results are modified using truncation / reduction of precision:
+              1) Truncate MBF to "points" only - i.e. FLOOR(best / 10000000)
+              2) Leave FMC "single" as a move count - i.e. best
+              3) Truncate everything else to the nearest second - i.e. FLOOR(best / 100)
 
-    Notes:    These extracts will never be shared publicly
+    Notes:    The extracts will not disclose any WCA IDs, regardless of whether they are already known
+              These extracts will never be shared publicly
 */
 
-/* 
-   Extract AGGREGATED senior results (averages)
-   
-   1) Output counts of seniors rather than WCA IDs
-   2) Truncate everything to the nearest second - i.e. FLOOR(best / 100)
-*/
-
-SELECT eventId, FLOOR(best_average / 100) AS modified_average, COUNT(*) AS num_persons
--- INTO OUTFILE '/home/jovyan/work/wca-ipy/data/private/extract/known_senior_averages_agg.csv' FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"'
+DROP TEMPORARY TABLE IF EXISTS senior_results;
+CREATE TEMPORARY TABLE senior_prs AS
+SELECT eventId, personId, FLOOR(age_at_comp / 10) * 10 AS age_category,
+  MIN(CASE WHEN eventId IN ('333mbf', '333mbo') THEN FLOOR(best / 10000000) WHEN eventId IN ('333fm') THEN best ELSE FLOOR(best / 100) END) AS modified_single,
+  MIN(IF(average > 0, FLOOR(average / 100), NULL)) AS modified_average
 FROM
 (
-  SELECT eventId, personId, MIN(average) AS best_average
-  FROM
-  (
-    SELECT r.eventId, r.personId, r.average,
-      TIMESTAMPDIFF(YEAR,
-        DATE_FORMAT(CONCAT(p.year, '-', p.month, '-', p.day), '%Y-%m-%d'),
-        DATE_FORMAT(CONCAT(c.year, '-', c.month, '-', c.day), '%Y-%m-%d')) AS age_at_comp
-    FROM Results AS r
-    INNER JOIN Competitions AS c ON r.competitionId = c.id
-    INNER JOIN Persons AS p ON r.personId = p.id AND p.subid = 1 AND p.year > 0 AND p.year <= YEAR(CURDATE()) - 40
-    WHERE average > 0
-    HAVING age_at_comp >= 40
-  ) AS tmp_results
-  GROUP BY eventId, personId
-) AS tmp_persons
+  SELECT r.eventId, r.personId, r.best, r.average,
+    TIMESTAMPDIFF(YEAR,
+      DATE_FORMAT(CONCAT(p.year, '-', p.month, '-', p.day), '%Y-%m-%d'),
+      DATE_FORMAT(CONCAT(c.year, '-', c.month, '-', c.day), '%Y-%m-%d')) AS age_at_comp
+  FROM Results AS r
+  INNER JOIN Competitions AS c ON r.competitionId = c.id
+  INNER JOIN Persons AS p ON r.personId = p.id AND p.subid = 1 AND p.year > 0 AND p.year <= YEAR(CURDATE()) - 40
+  WHERE best > 0
+  HAVING age_at_comp >= 40
+) AS senior_prs
+GROUP BY eventId, personId, age_category;
+
+-- Combine multiple queries for different age categories
+SELECT eventId, modified_average, 40 AS age_category, COUNT(DISTINCT personId) AS num_persons
+FROM senior_prs
+WHERE age_category >= 40
 GROUP BY eventId, modified_average
-ORDER BY eventId, modified_average;
+UNION ALL
+SELECT eventId, modified_average, 50 AS age_category, COUNT(DISTINCT personId) AS num_persons
+FROM senior_prs
+WHERE age_category >= 50
+GROUP BY eventId, modified_average
+UNION ALL
+SELECT eventId, modified_average, 60 AS age_category, COUNT(DISTINCT personId) AS num_persons
+FROM senior_prs
+WHERE age_category >= 60
+GROUP BY eventId, modified_average
+UNION ALL
+SELECT eventId, modified_average, 70 AS age_category, COUNT(DISTINCT personId) AS num_persons
+FROM senior_prs
+WHERE age_category >= 70
+GROUP BY eventId, modified_average
+UNION ALL
+SELECT eventId, modified_average, 80 AS age_category, COUNT(DISTINCT personId) AS num_persons
+FROM senior_prs
+WHERE age_category >= 80
+GROUP BY eventId, modified_average
+UNION ALL
+SELECT eventId, modified_average, 90 AS age_category, COUNT(DISTINCT personId) AS num_persons
+FROM senior_prs
+WHERE age_category >= 90
+GROUP BY eventId, modified_average
+ORDER BY eventId, modified_average, age_category;
 
-/* 
-   Extract AGGREGATED senior results (singles)
-   
-   1) Output counts of seniors rather than WCA IDs
-   2) Truncate MBF to "points" only - i.e. FLOOR(best / 10000000)
-   3) Leave FMC "single" as a move count - i.e. best
-   4) Truncate everything else to the nearest second - i.e. FLOOR(best / 100)
-*/
-
-SELECT eventId,
-  (
-    CASE
-      WHEN eventId IN ('333mbf', '333mbo') THEN FLOOR(best_single / 10000000)
-      WHEN eventId IN ('333fm') THEN best_single
-      ELSE FLOOR(best_single / 100)
-    END
-  ) AS modified_single, COUNT(*) AS num_persons
--- INTO OUTFILE '/home/jovyan/work/wca-ipy/data/private/extract/known_senior_singles_agg.csv' FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"'
-FROM
-(   
-  SELECT eventId, personId, MIN(best) AS best_single
-  FROM
-  (
-    SELECT r.eventId, r.personId, r.best,
-      TIMESTAMPDIFF(YEAR,
-        DATE_FORMAT(CONCAT(p.year, '-', p.month, '-', p.day), '%Y-%m-%d'),
-        DATE_FORMAT(CONCAT(c.year, '-', c.month, '-', c.day), '%Y-%m-%d')) AS age_at_comp
-    FROM Results AS r
-    INNER JOIN Competitions AS c ON r.competitionId = c.id
-    INNER JOIN Persons AS p ON r.personId = p.id AND p.subid = 1 AND p.year > 0 AND p.year <= YEAR(CURDATE()) - 40
-    WHERE best > 0
-    HAVING age_at_comp >= 40
-  ) AS tmp_results
-  GROUP BY eventId, personId
-) AS tmp_persons
+-- Combine multiple queries for different age categories
+SELECT eventId, modified_single, 40 AS age_category, COUNT(DISTINCT personId) AS num_persons
+FROM senior_prs
+WHERE age_category >= 40
 GROUP BY eventId, modified_single
-ORDER BY eventId, modified_single;
+UNION ALL
+SELECT eventId, modified_single, 50 AS age_category, COUNT(DISTINCT personId) AS num_persons
+FROM senior_prs
+WHERE age_category >= 50
+GROUP BY eventId, modified_single
+UNION ALL
+SELECT eventId, modified_single, 60 AS age_category, COUNT(DISTINCT personId) AS num_persons
+FROM senior_prs
+WHERE age_category >= 60
+GROUP BY eventId, modified_single
+UNION ALL
+SELECT eventId, modified_single, 70 AS age_category, COUNT(DISTINCT personId) AS num_persons
+FROM senior_prs
+WHERE age_category >= 70
+GROUP BY eventId, modified_single
+UNION ALL
+SELECT eventId, modified_single, 80 AS age_category, COUNT(DISTINCT personId) AS num_persons
+FROM senior_prs
+WHERE age_category >= 80
+GROUP BY eventId, modified_single
+UNION ALL
+SELECT eventId, modified_single, 90 AS age_category, COUNT(DISTINCT personId) AS num_persons
+FROM senior_prs
+WHERE age_category >= 90
+GROUP BY eventId, modified_single
+ORDER BY eventId, modified_single, age_category;
--- sql/extract_senior_results.sql
+++ sql/extract_senior_results.sql
@@ -1,63 +1,27 @@
 /* 
-    Script:   Extract Over 40s Results
-    Created:  2019-02-15
+    Script:   Extract Senior Results
+    Created:  2019-07-06
     Author:   Michael George / 2015GEOR02
    
     Purpose:  Private extract to facilitate the production of "Indicative Senior Rankings"
               https://logiqx.github.io/wca-ipy/Indicative_Rankings.html
 
-    Notes:    These extracts will never be shared publicly
+    Notes:    This extract will never be shared publicly
 */
 
--- Extract seniors
-SELECT personId, personName, MAX(ageCategory) AS ageCategory
--- INTO OUTFILE '/home/jovyan/work/wca-ipy/data/private/extract/known_senior_details.csv' FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"'
+-- Extract senior bests
+SELECT eventId, personId, FLOOR(age_at_comp / 10) * 10 AS age_category,
+  MIN(best) AS best_single, MIN(IF(average > 0, average, NULL)) AS best_average
 FROM
 (
-  SELECT r.eventId, r.personId, r.average, p.name AS PersonName,
-    FLOOR(TIMESTAMPDIFF(YEAR,
+  SELECT r.eventId, r.personId, r.best, r.average,
+    TIMESTAMPDIFF(YEAR,
       DATE_FORMAT(CONCAT(p.year, '-', p.month, '-', p.day), '%Y-%m-%d'),
-      DATE_FORMAT(CONCAT(c.year, '-', c.month, '-', c.day), '%Y-%m-%d')) / 10) * 10 AS ageCategory
+      DATE_FORMAT(CONCAT(c.year, '-', c.month, '-', c.day), '%Y-%m-%d')) AS age_at_comp
   FROM Results AS r
   INNER JOIN Competitions AS c ON r.competitionId = c.id
   INNER JOIN Persons AS p ON r.personId = p.id AND p.subid = 1 AND p.year > 0 AND p.year <= YEAR(CURDATE()) - 40
-  HAVING ageCategory >= 40
-) AS t
-GROUP BY personId
-ORDER BY personName, personId;
-
--- Extract senior results (averages)
-SELECT eventId, personId, MIN(average) AS bestAverage, ageCategory
--- INTO OUTFILE '/home/jovyan/work/wca-ipy/data/private/extract/known_senior_averages.csv' FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"'
-FROM
-(
-  SELECT r.eventId, r.personId, r.average, p.name AS personName,
-    FLOOR(TIMESTAMPDIFF(YEAR,
-      DATE_FORMAT(CONCAT(p.year, '-', p.month, '-', p.day), '%Y-%m-%d'),
-      DATE_FORMAT(CONCAT(c.year, '-', c.month, '-', c.day), '%Y-%m-%d')) / 10) * 10 AS ageCategory
-  FROM Results AS r
-  INNER JOIN Competitions AS c ON r.competitionId = c.id
-  INNER JOIN Persons AS p ON r.personId = p.id AND p.subid = 1 AND p.year > 0 AND p.year <= YEAR(CURDATE()) - 40
-  WHERE average > 0
-  HAVING ageCategory >= 40
-) AS t
-GROUP BY eventId, personId, ageCategory
-ORDER BY eventId, bestAverage, personId, ageCategory DESC;
-
--- Extract senior results (singles)
-SELECT eventId, personId, MIN(best) AS bestSingle, ageCategory
--- INTO OUTFILE '/home/jovyan/work/wca-ipy/data/private/extract/known_senior_singles.csv' FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"'
-FROM
-(
-  SELECT r.eventId, r.personId, r.best, p.name AS personName,
-    FLOOR(TIMESTAMPDIFF(YEAR,
-      DATE_FORMAT(CONCAT(p.year, '-', p.month, '-', p.day), '%Y-%m-%d'),
-      DATE_FORMAT(CONCAT(c.year, '-', c.month, '-', c.day), '%Y-%m-%d')) / 10) * 10 AS ageCategory
-  FROM Results AS r
-  INNER JOIN Competitions AS c ON r.competitionId = c.id
-  INNER JOIN Persons AS p ON r.personId = p.id AND p.year > 0 AND p.year <= YEAR(CURDATE()) - 40
   WHERE best > 0
-  HAVING ageCategory >= 40
-) AS tmp_results
-GROUP BY eventId, personId, ageCategory
-ORDER BY eventId, bestSingle, personId, ageCategory DESC;
+  HAVING age_at_comp >= 40
+) AS senior_results
+GROUP BY eventId, personId, age_category;
