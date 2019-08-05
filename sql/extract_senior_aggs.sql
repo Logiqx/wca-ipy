ee4b90ead60c4b95a20f73e3dbdf9bd3db41f4ce
Logiqx <software@mikeg.me.uk>
2019-08-05 11:45:06 +0100

    Re-factor code to make more readable

--- sql/extract_senior_aggs.sql
+++ sql/extract_senior_aggs.sql
@@ -15,35 +15,22 @@
    Create temporary table(s) containing "senior bests" - one record per person, per event, per age category
 */
 
--- Determine "explicit" senior bests - "age category" is based on "age at competition"
-DROP TEMPORARY TABLE IF EXISTS senior_bests_1;
-CREATE TEMPORARY TABLE senior_bests_1 AS
-SELECT personId, eventId, FLOOR(age_at_comp / 10) * 10 AS age_category,
-  MIN(best) AS best_single, MIN(IF(average > 0, average, NULL)) AS best_average
+DROP TEMPORARY TABLE IF EXISTS senior_bests;
+CREATE TEMPORARY TABLE senior_bests AS
+SELECT personId, eventId, age_category, MIN(best) AS best_single, MIN(IF(average > 0, average, NULL)) AS best_average
 FROM
 (
-  -- Derived table contains senior results, including age at the start of the competition
-  -- Index hint (USE INDEX) ensures that MySQL / MariaDB uses the optimal query execution plan
-  -- i.e. Persons ("WHERE" limits to seniors) -> Results.personId -> Competitions.id
-  SELECT r.personId, r.eventId, r.best, r.average,
-    TIMESTAMPDIFF(YEAR,
+  SELECT r.personId, r.eventId, r.best, r.average, TIMESTAMPDIFF(YEAR,
       DATE_FORMAT(CONCAT(p.year, '-', p.month, '-', p.day), '%Y-%m-%d'),
       DATE_FORMAT(CONCAT(c.year, '-', c.month, '-', c.day), '%Y-%m-%d')) AS age_at_comp
-  FROM Persons AS p USE INDEX ()
+  FROM Persons AS p
   JOIN Results AS r ON r.personId = p.id AND best > 0
   JOIN Competitions AS c ON c.id = r.competitionId
   WHERE p.year > 0 AND p.year <= YEAR(CURDATE()) - 40
   AND p.subid = 1
   HAVING age_at_comp >= 40
-) AS senior_results
-GROUP BY personId, eventId, age_category;
-
--- Determine "implicit" senior bests - e.g. "over 50" also counts as "over 40", etc
-DROP TEMPORARY TABLE IF EXISTS senior_bests_2;
-CREATE TEMPORARY TABLE senior_bests_2 AS
-SELECT personId, eventId, a.age_category, MIN(best_single) AS best_single, MIN(best_average) AS best_average
-FROM senior_bests_1 AS s
-JOIN (SELECT 40 AS age_category UNION ALL SELECT 50 UNION ALL SELECT 60 UNION ALL SELECT 70 UNION ALL SELECT 80 UNION ALL SELECT 90 UNION ALL SELECT 100) AS a ON a.age_category <= s.age_category
+) AS t
+JOIN (SELECT 40 AS age_category UNION ALL SELECT 50 UNION ALL SELECT 60 UNION ALL SELECT 70 UNION ALL SELECT 80 UNION ALL SELECT 90 UNION ALL SELECT 100) AS a ON age_category <= age_at_comp
 GROUP BY personId, eventId, age_category;
 
 /*
@@ -52,11 +39,10 @@ GROUP BY personId, eventId, age_category;
    2) Truncate everything to the nearest second - i.e. FLOOR(best / 100)
 */
 
--- Averages are divided by 100 for all events (i.e. truncated to the nearest second)
 SELECT eventId, age_category,
   FLOOR(best_average / 100) AS modified_average,
   COUNT(*) AS num_persons
-FROM senior_bests_2
+FROM senior_bests
 WHERE best_average > 0
 GROUP BY eventId, age_category, modified_average
 ORDER BY eventId, age_category, modified_average;
@@ -69,7 +55,6 @@ ORDER BY eventId, age_category, modified_average;
    4) Truncate everything else to the nearest second - i.e. FLOOR(best / 100)
 */
 
--- Singles need to be handled slightly differently to averages
 SELECT eventId, age_category,
   (
     CASE WHEN eventId IN ('333mbf', '333mbo') THEN FLOOR(best_single / 10000000)
@@ -78,6 +63,6 @@ SELECT eventId, age_category,
     END
   ) AS modified_single,
   COUNT(*) AS num_persons
-FROM senior_bests_2
+FROM senior_bests
 GROUP BY eventId, age_category, modified_single
 ORDER BY eventId, age_category, modified_single;
