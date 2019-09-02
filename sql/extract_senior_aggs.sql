89e8447a434ca62d1d6efeac316d5188cf08099e
Logiqx <software@mikeg.me.uk>
2019-09-02 08:56:22 +0100

    Re-factor for environments where temporary tables are not permitted

--- sql/extract_senior_aggs.sql
+++ sql/extract_senior_aggs.sql
@@ -13,28 +13,6 @@
 */
 
 /*
-   Create temporary table(s) containing "senior bests" - one record per person, per event, per age category
-*/
-
-DROP TEMPORARY TABLE IF EXISTS senior_bests;
-CREATE TEMPORARY TABLE senior_bests AS
-SELECT personId, eventId, age_category, MIN(best) AS best_single, MIN(IF(average > 0, average, NULL)) AS best_average
-FROM
-(
-  SELECT r.personId, r.eventId, r.best, r.average, TIMESTAMPDIFF(YEAR,
-      DATE_FORMAT(CONCAT(p.year, '-', p.month, '-', p.day), '%Y-%m-%d'),
-      DATE_FORMAT(CONCAT(c.year, '-', c.month, '-', c.day), '%Y-%m-%d')) AS age_at_comp
-  FROM Persons AS p
-  JOIN Results AS r ON r.personId = p.id AND best > 0
-  JOIN Competitions AS c ON c.id = r.competitionId
-  WHERE p.year > 0 AND p.year <= YEAR(CURDATE()) - 40
-  AND p.subid = 1
-  HAVING age_at_comp >= 40
-) AS t
-JOIN (SELECT 40 AS age_category UNION ALL SELECT 50 UNION ALL SELECT 60 UNION ALL SELECT 70 UNION ALL SELECT 80 UNION ALL SELECT 90 UNION ALL SELECT 100) AS a ON age_category <= age_at_comp
-GROUP BY personId, eventId, age_category;
-
-/*
    Extract AGGREGATED senior results (averages)
    1) Output counts of seniors rather than WCA IDs
    2) Truncate everything to the nearest second - i.e. FLOOR(best / 100)
@@ -43,7 +21,33 @@ GROUP BY personId, eventId, age_category;
 SELECT eventId, age_category,
   FLOOR(best_average / 100) AS modified_average,
   COUNT(*) AS num_persons
-FROM senior_bests
+FROM
+(
+  SELECT personId, eventId, age_category, MIN(average) AS best_average
+  FROM
+  (
+    SELECT r.personId, r.eventId, r.best, r.average, TIMESTAMPDIFF(YEAR,
+      DATE_FORMAT(CONCAT(p.year, '-', p.month, '-', p.day), '%Y-%m-%d'),
+      DATE_FORMAT(CONCAT(c.year, '-', c.month, '-', c.day), '%Y-%m-%d')) AS age_at_comp
+    FROM Persons AS p
+    JOIN Results AS r ON r.personId = p.id AND average > 0
+    JOIN Competitions AS c ON c.id = r.competitionId
+    WHERE p.year > 0 AND p.year <= YEAR(CURDATE()) - 40
+    AND p.subid = 1
+    HAVING age_at_comp >= 40
+  ) AS t
+  JOIN
+  (
+    SELECT 40 AS age_category
+    UNION ALL SELECT 50
+    UNION ALL SELECT 60
+    UNION ALL SELECT 70
+    UNION ALL SELECT 80
+    UNION ALL SELECT 90
+    UNION ALL SELECT 100
+  ) AS age_categories ON age_category <= age_at_comp
+  GROUP BY personId, eventId, age_category
+) AS senior_bests
 WHERE best_average > 0
 GROUP BY eventId, age_category, modified_average
 ORDER BY eventId, age_category, modified_average;
@@ -64,6 +68,32 @@ SELECT eventId, age_category,
     END
   ) AS modified_single,
   COUNT(*) AS num_persons
-FROM senior_bests
+FROM
+(
+  SELECT personId, eventId, age_category, MIN(best) AS best_single
+  FROM
+  (
+    SELECT r.personId, r.eventId, r.best, r.average, TIMESTAMPDIFF(YEAR,
+      DATE_FORMAT(CONCAT(p.year, '-', p.month, '-', p.day), '%Y-%m-%d'),
+      DATE_FORMAT(CONCAT(c.year, '-', c.month, '-', c.day), '%Y-%m-%d')) AS age_at_comp
+    FROM Persons AS p
+    JOIN Results AS r ON r.personId = p.id AND best > 0
+    JOIN Competitions AS c ON c.id = r.competitionId
+    WHERE p.year > 0 AND p.year <= YEAR(CURDATE()) - 40
+    AND p.subid = 1
+    HAVING age_at_comp >= 40
+  ) AS t
+  JOIN
+  (
+    SELECT 40 AS age_category
+    UNION ALL SELECT 50
+    UNION ALL SELECT 60
+    UNION ALL SELECT 70
+    UNION ALL SELECT 80
+    UNION ALL SELECT 90
+    UNION ALL SELECT 100
+  ) AS age_categories ON age_category <= age_at_comp
+  GROUP BY personId, eventId, age_category
+) AS senior_bests
 GROUP BY eventId, age_category, modified_single
 ORDER BY eventId, age_category, modified_single;
