8be74ff6d51c7d1a7c569433129e88ce4cfce0ff
Logiqx <software@mikeg.me.uk>
2019-12-14 14:32:31 +0000

    Use set -e

--- bin/build.sh
+++ bin/build.sh
@@ -1,5 +1,7 @@
 . $(dirname $0)/env.sh
 
+set -e
+
 cd $PROJ_DIR
 
 DOCKER_BUILDKIT=1 docker build . -t $PROJ_NAME:$(git rev-parse --short=12 HEAD)
--- bin/rankings.sh
+++ bin/rankings.sh
@@ -1,5 +1,7 @@
 . $(dirname $0)/env.sh
 
+set -e
+
 time run_py_script Partial_Rankings.py; echo
 time run_py_script Indicative_Rankings.py; echo
 time run_py_script Representative_Rankings.py; echo
