5a040c7eafcdf198eaac3ba7282810251475443b
Logiqx <software@mikeg.me.uk>
2019-12-17 12:47:36 +0000

    Remove unnecessary schema name

--- sql/create_senior_estimates.sql
+++ sql/create_senior_estimates.sql
@@ -16,10 +16,10 @@ SELECT rs.eventId, s1. ageCategory,
     s1.numSingles AS prevSingles, s1.numAverages AS prevAverages, FLOOR(DATEDIFF(CURDATE(), s1.cutoffDate) / 7) AS numWeeks
 FROM (SELECT eventId, COUNT(DISTINCT personId) AS numSingles FROM wca.RanksSingle GROUP BY eventId) AS rs
 LEFT JOIN (SELECT eventId, COUNT(DISTINCT personId) AS numAverages FROM wca.RanksAverage GROUP BY eventId) AS ra ON rs.eventId = ra.eventId
-JOIN wca_ipy.WcaResultCounts w1 ON w1.eventId = rs.eventId AND w1.cutoffDate = '2019-08-10'
-JOIN wca_ipy.WcaResultCounts w2 ON w2.eventId = rs.eventId AND w2.cutoffDate = '2019-02-01'
-JOIN wca_ipy.SeniorResultCounts s1 ON s1.eventId = rs.eventId AND s1.cutoffDate = w1.cutoffDate
-LEFT JOIN wca_ipy.SeniorResultCounts s2 ON s2.eventId = rs.eventId AND s2.cutoffDate = w2.cutoffDate AND s2.ageCategory = s1.ageCategory;
+JOIN WcaResultCounts w1 ON w1.eventId = rs.eventId AND w1.cutoffDate = '2019-08-10'
+JOIN WcaResultCounts w2 ON w2.eventId = rs.eventId AND w2.cutoffDate = '2019-02-01'
+JOIN SeniorResultCounts s1 ON s1.eventId = rs.eventId AND s1.cutoffDate = w1.cutoffDate
+LEFT JOIN SeniorResultCounts s2 ON s2.eventId = rs.eventId AND s2.cutoffDate = w2.cutoffDate AND s2.ageCategory = s1.ageCategory;
 
 -- Increase estimates where more seniors are already known
 UPDATE SeniorEstimates AS e
