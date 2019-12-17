2f7093b19741e5f50495f92b5ec61fd54221ebb2
Logiqx <software@mikeg.me.uk>
2019-12-17 10:42:47 +0000

    Increase estimates where more seniors are already known

--- sql/create_senior_estimates.sql
+++ sql/create_senior_estimates.sql
@@ -8,6 +8,7 @@
 
 DROP TABLE IF EXISTS SeniorEstimates;
 
+-- Start by calculating estimates
 CREATE TABLE SeniorEstimates AS
 SELECT rs.eventId, s1. ageCategory,
     FLOOR(s1.numSingles * rs.numSingles / w1.numSingles * IF(s2.numAverages, (s1.numAverages / s2.numAverages) / (w1.numAverages / w2.numAverages), 1)) AS estSingles, -- tmp
@@ -19,3 +20,27 @@ JOIN wca_ipy.WcaResultCounts w1 ON w1.eventId = rs.eventId AND w1.cutoffDate = '
 JOIN wca_ipy.WcaResultCounts w2 ON w2.eventId = rs.eventId AND w2.cutoffDate = '2019-02-01'
 JOIN wca_ipy.SeniorResultCounts s1 ON s1.eventId = rs.eventId AND s1.cutoffDate = w1.cutoffDate
 LEFT JOIN wca_ipy.SeniorResultCounts s2 ON s2.eventId = rs.eventId AND s2.cutoffDate = w2.cutoffDate AND s2.ageCategory = s1.ageCategory;
+
+-- Increase estimates where more seniors are already known
+UPDATE SeniorEstimates AS e
+JOIN
+(
+    SELECT eventId, age_category, COUNT(DISTINCT personId) AS num_singles, COUNT(DISTINCT IF(average > 0, personId, NULL)) AS num_averages
+    FROM
+    (
+      SELECT r.personId, r.eventId, r.average, TIMESTAMPDIFF(YEAR,
+          DATE_FORMAT(CONCAT(p.year, '-', p.month, '-', p.day), '%Y-%m-%d'),
+          DATE_FORMAT(CONCAT(c.year, '-', c.month, '-', c.day), '%Y-%m-%d')) AS age_at_comp
+      FROM wca.Persons AS p
+      JOIN wca.Results AS r ON r.personId = p.id AND best > 0
+      JOIN wca.Competitions AS c ON c.id = r.competitionId
+      WHERE p.year > 0 AND p.year <= YEAR(CURDATE()) - 40
+      AND p.subid = 1
+      HAVING age_at_comp >= 40
+    ) AS t
+    JOIN (SELECT 40 AS age_category UNION ALL SELECT 50 UNION ALL SELECT 60 UNION ALL SELECT 70 UNION ALL SELECT 80 UNION ALL SELECT 90 UNION ALL SELECT 100) AS a ON age_category <= age_at_comp
+    GROUP BY eventId, age_category
+    ORDER BY eventId, age_category
+) AS t ON t.eventId = e.eventId AND t.age_category = e.ageCategory
+SET e.estSingles = IF(estSingles > num_singles, estSingles, num_singles),
+    e.estAverages = IF(estAverages > num_averages, estAverages, num_averages);
