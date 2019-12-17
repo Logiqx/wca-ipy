2bc9d3ffd9ab83823dfd09c9b43c4ec967ded54b
Logiqx <software@mikeg.me.uk>
2019-12-17 21:25:29 +0000

    Use continent codes

--- sql/extract_wca_lookups.sql
+++ sql/extract_wca_lookups.sql
@@ -11,10 +11,12 @@ FROM wca.Events
 WHERE `rank` < 900
 ORDER BY `rank`;
 
-SELECT 'wca_lookup_countries', id, name, continentId, iso2
-FROM wca.Countries
-ORDER BY id;
+SELECT 'wca_lookup_countries', iso2 AS id, name, cc AS continentId
+FROM wca.Countries AS c
+JOIN ContinentCodes AS cc ON cc.id = c.continentId
+ORDER BY name;
 
-SELECT 'wca_lookup_continents', id, name
-FROM wca.Continents
-ORDER BY id;
+SELECT 'wca_lookup_continents', cc AS id, name
+FROM wca.Continents AS c
+JOIN ContinentCodes AS cc ON cc.id = c.id
+ORDER BY name;
