206e0f9982c3294ca12a64505e32a521f883a560
Logiqx <software@mikeg.me.uk>
2019-12-17 21:40:00 +0000

    Add continent and country filter

--- docs/js/rankings.js
+++ docs/js/rankings.js
@@ -1,17 +1,25 @@
 //
 // Render options as drop-down lists
 //
-function renderOptions(eventId, resultType, ageCategory, width)
+function renderOptions(eventId, resultType, ageCategory, continentId, countryId, width)
 {
+	var personIds = getPersonIds();
+	var countryIds = getCountryIds();
+	var continentIds = getContinentIds();
+
 	var out = "";
 
-	if (width >= GALAXY_S3_PORTRAIT)
+	if (width >= GALAXY_S3_LANDSCAPE)
+	{
+		out += "<div class=\"dropdowns\"><p><table><tr><th>Event:</th><th>Result:</th><th>Age:</th><th>Continent:</th><th>Country:</th></tr><tr>";
+	}
+	else if (width >= GALAXY_S3_PORTRAIT)
 	{
 		out += "<div class=\"dropdowns\"><p><table><tr><th>Event:</th><th>Result:</th><th>Age:</th></tr><tr>";
 	}
 	
 	// Create select element for available events
-	var filteredEvents = filterEventIds(resultType, ageCategory);
+	var filteredEvents = filterEventIds(resultType, ageCategory, continentId, countryId);
 	if (width < GALAXY_S3_PORTRAIT)
 	{
 		out += "<div class=\"dropdowns\"><table><tr><th>Event:</th>";
@@ -30,7 +38,7 @@ function renderOptions(eventId, resultType, ageCategory, width)
 	out += "</select></td>";
 	
 	// Create select element for available result types
-	var resultTypes = filterResultTypes(eventId, ageCategory);
+	var resultTypes = filterResultTypes(eventId, ageCategory, continentId, countryId);
 	if (width < GALAXY_S3_PORTRAIT)
 	{
 		out += "</tr><tr><th>Result:</th>";
@@ -48,7 +56,7 @@ function renderOptions(eventId, resultType, ageCategory, width)
 	out += "</select></td>";
 
 	// Create select element for available age categories
-	var ageCategories = filterAgeCategories(eventId, resultType);
+	var ageCategories = filterAgeCategories(eventId, resultType, continentId, countryId);
 	if (width < GALAXY_S3_PORTRAIT)
 	{
 		out += "</tr><tr><th>Age:</th>";
@@ -63,6 +71,69 @@ function renderOptions(eventId, resultType, ageCategory, width)
 		}
 		out += ">Over " + ageCategories[i] + "</option>";
 	}
+	out += "</select></td>";
+
+	if (width < GALAXY_S3_LANDSCAPE && width >= GALAXY_S3_PORTRAIT)
+	{
+		out += "</tr></table></p></div><div class=\"dropdowns\"><p><table><tr><th>Continent:</th><th>Country:</th></tr><tr>";
+	}
+
+	// Create select element for available continents
+	var filteredContinents = filterContinents(eventId, resultType, ageCategory, countryId);
+	if (width < GALAXY_S3_PORTRAIT)
+	{
+		out += "</tr><tr><th>Continent:</th>";
+	}
+	out += "<td><select id=\"continentId\" onChange=\"switchView()\">";
+	out += "<option value=\"xx\"";
+	if (continentId == "xx")
+	{
+		out += " selected";
+	}
+	out += "></option>";
+	for (var i = 0; i < continentIds.length; i++)
+	{
+		if (filteredContinents.indexOf(continentIds[i]) >= 0)
+		{
+			var continentObj = rankings.continents[i];
+
+			out += "<option value=\"" + rankings.continents[i].id + "\"";
+			if (rankings.continents[i].id == continentId)
+			{
+				out += " selected";
+			}
+			out += ">" + rankings.continents[i].name + "</option>";
+		}
+	}
+	out += "</select></td>";
+
+	// Create select element for available countries
+	var filteredCountries = filterCountries(eventId, resultType, ageCategory, continentId);
+	if (width < GALAXY_S3_PORTRAIT)
+	{
+		out += "</tr><tr><th>Country:</th>";
+	}
+	out += "<td><select id=\"countryId\" onChange=\"switchView()\">";
+	out += "<option value=\"xx\"";
+	if (countryId == "xx")
+	{
+		out += " selected";
+	}
+	out += "></option>";
+	for (var i = 0; i < countryIds.length; i++)
+	{
+		if (filteredCountries.indexOf(countryIds[i]) >= 0)
+		{
+			var countryObj = rankings.countries[i];
+
+			out += "<option value=\"" + rankings.countries[i].id + "\"";
+			if (rankings.countries[i].id == countryId)
+			{
+				out += " selected";
+			}
+			out += ">" + rankings.countries[i].name + "</option>";
+		}
+	}
 	out += "</select></td></tr></table></p></div>";
 
 	return out;
@@ -71,7 +142,7 @@ function renderOptions(eventId, resultType, ageCategory, width)
 //
 // List events related to result type and age category
 //
-function filterEventIds(resultType, ageCategory)
+function filterEventIds(resultType, ageCategory, continentId, countryId)
 {
 	var eventIds = [];
 
@@ -82,10 +153,27 @@ function filterEventIds(resultType, ageCategory)
 		for (var rankingIdx = 0; rankingIdx < rankings.events[eventIdx].rankings.length; rankingIdx++)
 		{
 			var rankingObj = rankings.events[eventIdx].rankings[rankingIdx];
-			
-			if (rankingObj.type == resultType && rankingObj.age == ageCategory && eventIds.indexOf(eventObj.id) < 0)
+
+			if (rankingObj.type == resultType && rankingObj.age == ageCategory)
 			{
-				eventIds.push(eventObj.id)
+				for (var rankIdx = 0; rankIdx < rankingObj.ranks.length; rankIdx++)
+				{
+					var rankObj = rankingObj.ranks[rankIdx];
+					var personObj = rankings.persons[personIds.indexOf(rankObj.id)]
+					var countryObj = rankings.countries[countryIds.indexOf(personObj.country)]
+
+					if ((continentId == "XX" || countryObj.continent == continentId) &&
+						(countryId == "XX" || personObj.country == countryId))
+					{
+						eventIds.push(eventObj.id);
+						break;
+					}
+				}
+			}
+			
+			if (eventIds.indexOf(eventObj.id) >= 0)
+			{
+				break;
 			}
 		}
 	}
@@ -96,7 +184,7 @@ function filterEventIds(resultType, ageCategory)
 //
 // List result types related to event and age category
 //
-function filterResultTypes(eventId, ageCategory)
+function filterResultTypes(eventId, ageCategory, continentId, countryId)
 {
 	var resultTypes = [];
 	
@@ -108,7 +196,19 @@ function filterResultTypes(eventId, ageCategory)
 		
 		if (rankingObj.age == ageCategory && resultTypes.indexOf(rankingObj.type) < 0)
 		{
-			resultTypes.push(rankingObj.type)
+			for (var rankIdx = 0; rankIdx < rankingObj.ranks.length; rankIdx++)
+			{
+				var rankObj = rankingObj.ranks[rankIdx];
+				var personObj = rankings.persons[personIds.indexOf(rankObj.id)]
+				var countryObj = rankings.countries[countryIds.indexOf(personObj.country)]
+
+				if ((continentId == "XX" || countryObj.continent == continentId) &&
+					(countryId == "XX" || personObj.country == countryId))
+				{
+					resultTypes.push(rankingObj.type);
+					break;
+				}
+			}
 		}
 	}
 
@@ -123,7 +223,7 @@ function filterResultTypes(eventId, ageCategory)
 //
 // List age categories related to event and result type
 //
-function filterAgeCategories(eventId, resultType)
+function filterAgeCategories(eventId, resultType, continentId, countryId)
 {
 	var ageCategories = [];
 	
@@ -135,7 +235,19 @@ function filterAgeCategories(eventId, resultType)
 		
 		if (rankingObj.type == resultType && ageCategories.indexOf(rankingObj.age) < 0)
 		{
-			ageCategories.push(rankingObj.age)
+			for (var rankIdx = 0; rankIdx < rankingObj.ranks.length; rankIdx++)
+			{
+				var rankObj = rankingObj.ranks[rankIdx];
+				var personObj = rankings.persons[personIds.indexOf(rankObj.id)]
+				var countryObj = rankings.countries[countryIds.indexOf(personObj.country)]
+
+				if ((continentId == "XX" || countryObj.continent == continentId) &&
+					(countryId == "XX" || personObj.country == countryId))
+				{
+					ageCategories.push(rankingObj.age);
+					break;
+				}
+			}
 		}
 	}
 
@@ -143,17 +255,90 @@ function filterAgeCategories(eventId, resultType)
 }
 
 //
-// List age categories related to event and result type
+// List countries related to event, result type and age category
 //
-function renderTable(eventId, resultType, ageCategory, width)
+function filterContinents(eventId, resultType, ageCategory, countryId)
+{
+	var continents = [];
+
+	var eventObj = rankings.events[getEventIds().indexOf(eventId)];
+
+	for (var rankingIdx = 0; rankingIdx < eventObj.rankings.length; rankingIdx++)
+	{
+		var rankingObj = eventObj.rankings[rankingIdx];
+
+		if (rankingObj.type == resultType && rankingObj.age == ageCategory)
+		{
+			for (var rankIdx = 0; rankIdx < rankingObj.ranks.length; rankIdx++)
+			{
+				var rankObj = rankingObj.ranks[rankIdx];
+				var personObj = rankings.persons[personIds.indexOf(rankObj.id)]
+				var countryObj = rankings.countries[countryIds.indexOf(personObj.country)]
+
+				if ((countryId == "XX" || personObj.country == countryId) &&
+					continents.indexOf(countryObj.continent) < 0)
+				{
+					continents.push(countryObj.continent);
+				}
+			}
+		}
+	}
+
+	return continents;
+}
+
+//
+// List countries related to event, result type and age category
+//
+function filterCountries(eventId, resultType, ageCategory, continentId)
+{
+	var countries = [];
+
+	var eventObj = rankings.events[getEventIds().indexOf(eventId)];
+
+	for (var rankingIdx = 0; rankingIdx < eventObj.rankings.length; rankingIdx++)
+	{
+		var rankingObj = eventObj.rankings[rankingIdx];
+
+		if (rankingObj.type == resultType && rankingObj.age == ageCategory)
+		{
+			for (var rankIdx = 0; rankIdx < rankingObj.ranks.length; rankIdx++)
+			{
+				var rankObj = rankingObj.ranks[rankIdx];
+				var personObj = rankings.persons[personIds.indexOf(rankObj.id)]
+				var countryObj = rankings.countries[countryIds.indexOf(personObj.country)]
+
+				if ((continentId == "XX" || countryObj.continent == continentId) &&
+					countries.indexOf(countryObj.id) < 0)
+				{
+					countries.push(countryObj.id);
+				}
+			}
+		}
+	}
+
+	return countries;
+}
+
+//
+// Render results in a simple table
+//
+function renderTable(eventId, resultType, ageCategory, continentId, countryId, width)
 {
 	var out = "";
 
 	var personIds = getPersonIds();
 	var countryIds = getCountryIds();
+	var continentIds = getContinentIds();
 
 	var eventObj = rankings.events[getEventIds().indexOf(eventId)];
 	
+	if (continentId != "XX" || countryId != "XX")
+	{
+		out += '<p class="important">IMPORTANT: The continent and country filters do not provide any information relating to missing seniors.</p>';
+		out += '<p class="important">This information should should only be regarded as the relative positions of known seniors, hence the lack of any numbering.</p>';
+	}
+
 	for (var rankingIdx = 0; rankingIdx < eventObj.rankings.length; rankingIdx++)
 	{
 		var rankingObj = eventObj.rankings[rankingIdx];
@@ -162,7 +347,10 @@ function renderTable(eventId, resultType, ageCategory, width)
 		{
 			out += '<div class=\"rankings\"><table>';
 			out += '<tr>';
-			out += '<th class=\"rank\">Rank</th>';
+			if (continentId == "XX" && countryId == "XX")
+			{
+				out += '<th class=\"rank\">Rank</th>';
+			}
 			out += '<th>Person</th>';
 			if (width >= IPHONE_LANDSCAPE)
 			{
@@ -180,22 +368,29 @@ function renderTable(eventId, resultType, ageCategory, width)
 				var personObj = rankings.persons[personIds.indexOf(rankObj.id)]
 				var countryObj = rankings.countries[countryIds.indexOf(personObj.country)]
 
-				out += '<tr>';
-				out += '<td class=\"rank\">' + (rankObj.rank > prevRank ? rankObj.rank : '') + '</td>';
-				var href = '<a href=\"https://www.worldcubeassociation.org/persons/' + rankObj.id + '?event=' + eventId + '\">' + personObj.name + '</a>';
-				if (width >= IPHONE_LANDSCAPE)
+				if ((continentId == "XX" || countryObj.continent == continentId) &&
+					(countryId == "XX" || countryObj.id == countryId))
 				{
-					out += '<td>' + href + (rankObj.hasOwnProperty("age") ? ', ' + rankObj.age + '+' : '') + '</td>';
-					out += '<td>' + countryObj.name + '</td>';
-				}
-				else
-				{
-					out += '<td>' + href + ', ' + countryObj.name + (rankObj.hasOwnProperty("age") ? ', ' + rankObj.age + '+' : '') + '</td>';
-				}
-				out += '<td class=\"result\">' + rankObj.best + '</td>';
-				out += '</tr>';
+					out += '<tr>';
+					if (continentId == "XX" && countryId == "XX")
+					{
+						out += '<td class=\"rank\">' + (rankObj.rank > prevRank ? rankObj.rank : '') + '</td>';
+					}
+					var href = '<a href=\"https://www.worldcubeassociation.org/persons/' + rankObj.id + '?event=' + eventId + '\">' + personObj.name + '</a>';
+					if (width >= IPHONE_LANDSCAPE)
+					{
+						out += '<td>' + href + (rankObj.hasOwnProperty("age") ? ', ' + rankObj.age + '+' : '') + '</td>';
+						out += '<td>' + countryObj.name + '</td>';
+					}
+					else
+					{
+						out += '<td>' + href + ', ' + countryObj.name + (rankObj.hasOwnProperty("age") ? ', ' + rankObj.age + '+' : '') + '</td>';
+					}
+					out += '<td class=\"result\">' + rankObj.best + '</td>';
+					out += '</tr>';
 
-				prevRank = rankObj.rank;
+					prevRank = rankObj.rank;
+				}
 				count += 1;
 			}
 
@@ -203,8 +398,11 @@ function renderTable(eventId, resultType, ageCategory, width)
 
 			if (rankingObj.hasOwnProperty("estimate"))
 			{
-				out += '<p>Estimated number of seniors &#8776; ' + rankingObj.estimate + '</p>';
-				out += '<p>Estimated completeness of rankings &#8776; ' + (100 * count / rankingObj.estimate).toFixed(1) + '%</p>';
+				if (continentId == "XX" && countryId == "XX")
+				{
+					out += '<p>Estimated number of seniors &#8776; ' + rankingObj.estimate + '</p>';
+					out += '<p>Estimated completeness of rankings &#8776; ' + (100 * count / rankingObj.estimate).toFixed(1) + '%</p>';
+				}
 			}
 		}
 	}
@@ -225,8 +423,10 @@ function renderRankings(hashParts, width)
 	
 	// Determine the event
 	var eventId = hashParts[0].length > 0 ? hashParts[0] : eventIds[0];
-	var resultType = hashParts.length > 1 && hashParts[1].length > 0 ? hashParts[1] : getResultTypes()[0];
+	var resultType = hashParts.length > 1 && hashParts[1].length > 0 ? hashParts[1].toLowerCase() : getResultTypes()[0];
 	var ageCategory = hashParts.length > 2 && hashParts[2].length > 0 ? hashParts[2] : "40";
+	var continentId = hashParts.length > 3 && hashParts[3].length > 0 ? hashParts[3].toUpperCase() : "XX";
+	var countryId = hashParts.length > 4 && hashParts[4].length > 0 ? hashParts[4].toUpperCase() : "XX";
 	
 	// Check if the event exists
 	if (eventIds.indexOf(eventId) >= 0)
@@ -236,8 +436,8 @@ function renderRankings(hashParts, width)
 		document.title = eventObj.name + " - Over " + ageCategory + "s";
 
 		out += header();
-		out += renderOptions(eventId, resultType, ageCategory, width);
-		out += renderTable(eventId, resultType, ageCategory, width);
+		out += renderOptions(eventId, resultType, ageCategory, continentId, countryId, width);
+		out += renderTable(eventId, resultType, ageCategory, continentId, countryId, width);
 		out += footer();	
 	}
 	else
