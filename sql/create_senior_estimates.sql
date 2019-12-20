3e43a3bb7b9157cceebd59b5581619d56af643c9
Logiqx <software@mikeg.me.uk>
2019-12-20 12:20:31 +0000

    Simplify estimation process

--- sql/create_senior_estimates.sql
+++ sql/create_senior_estimates.sql
@@ -10,16 +10,14 @@ DROP TABLE IF EXISTS SeniorEstimates;
 
 -- Start by calculating estimates
 CREATE TABLE SeniorEstimates AS
-SELECT rs.eventId, s1. ageCategory,
-    FLOOR(s1.numSingles * rs.numSingles / w1.numSingles * IF(s2.numAverages, (s1.numAverages / s2.numAverages) / (w1.numAverages / w2.numAverages), 1)) AS estSingles, -- tmp
-    IFNULL(FLOOR(s1.numAverages * ra.numAverages / w1.numAverages * IF(s2.numAverages, (s1.numAverages / s2.numAverages) / (w1.numAverages / w2.numAverages), 1)), 0) AS estAverages,
-    s1.numSingles AS prevSingles, s1.numAverages AS prevAverages, FLOOR(DATEDIFF(CURDATE(), s1.cutoffDate) / 7) AS numWeeks
+SELECT rs.eventId, src.ageCategory,
+    FLOOR(src.numSingles * rs.numSingles / wrc.numSingles * rs.numSingles / wrc.numSingles) AS estSingles,
+    IFNULL(FLOOR(src.numAverages * ra.numAverages / wrc.numAverages * ra.numAverages / wrc.numAverages), 0) AS estAverages,
+    src.numSingles AS prevSingles, src.numAverages AS prevAverages, FLOOR(DATEDIFF(CURDATE(), src.cutoffDate) / 7) AS numWeeks
 FROM (SELECT eventId, COUNT(DISTINCT personId) AS numSingles FROM wca.RanksSingle GROUP BY eventId) AS rs
 LEFT JOIN (SELECT eventId, COUNT(DISTINCT personId) AS numAverages FROM wca.RanksAverage GROUP BY eventId) AS ra ON rs.eventId = ra.eventId
-JOIN WcaResultCounts w1 ON w1.eventId = rs.eventId AND w1.cutoffDate = '2019-08-10'
-JOIN WcaResultCounts w2 ON w2.eventId = rs.eventId AND w2.cutoffDate = '2019-02-01'
-JOIN SeniorResultCounts s1 ON s1.eventId = rs.eventId AND s1.cutoffDate = w1.cutoffDate
-LEFT JOIN SeniorResultCounts s2 ON s2.eventId = rs.eventId AND s2.cutoffDate = w2.cutoffDate AND s2.ageCategory = s1.ageCategory;
+JOIN WcaResultCounts AS wrc ON wrc.eventId = rs.eventId AND wrc.cutoffDate = '2019-12-20'
+JOIN SeniorResultCounts AS src ON src.eventId = rs.eventId AND src.cutoffDate = wrc.cutoffDate;
 
 -- Increase estimates where more seniors are already known
 UPDATE SeniorEstimates AS e
