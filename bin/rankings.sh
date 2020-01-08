f7e998c8e4ee98a7288daca54bfdb337c4c600f8
Logiqx <software@mikeg.me.uk>
2020-01-08 10:56:24 +0000

    Add senior rankings

--- bin/rankings.sh
+++ bin/rankings.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+time run_py_script Senior_Rankings.py; echo
 time run_py_script Partial_Rankings.py; echo
 time run_py_script Indicative_Rankings.py; echo
 time run_py_script Representative_Rankings.py; echo
