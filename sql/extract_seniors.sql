82184047bf49a9999330313c3a84d541a38c6bad
Logiqx <software@mikeg.me.uk>
2020-01-09 17:52:35 +0000

    Ensure name, countryId and gender are updated

--- sql/extract_seniors.sql
+++ sql/extract_seniors.sql
@@ -6,6 +6,10 @@
     Purpose:  Extract name, country, age category and details for speedsolving.com
 */
 
+UPDATE Seniors AS s
+JOIN wca.Persons AS p ON p.id = s.personId AND p.subid = 1
+SET s.name = p.name, s.countryId = p.countryId, s.gender = p.gender;
+
 SELECT t.personId, s.name AS personName, c.iso2 AS country, s.username, s.usernum, MAX(t.ageCategory) AS ageCategory, s.hidden, IFNULL(s.userId, 0) AS userId
 FROM
 (
