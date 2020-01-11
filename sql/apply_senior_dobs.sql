043e6e44073a80867ebf70bec2e2b35f2cfec8c3
Logiqx <software@mikeg.me.uk>
2020-01-11 10:46:47 +0000

    Check age categories

--- sql/apply_senior_dobs.sql
+++ sql/apply_senior_dobs.sql
@@ -18,3 +18,15 @@ INNER JOIN Seniors AS s ON s.personId = p.id AND s.dob IS NOT NULL
 SET p.year = DATE_FORMAT(s.dob, '%Y'),
     p.month = DATE_FORMAT(s.dob, '%m'),
     p.day = DATE_FORMAT(s.dob, '%d');
+
+-- Check age categories
+SELECT FLOOR(TIMESTAMPDIFF(YEAR,
+	DATE_FORMAT(CONCAT(p.year, '-', p.month, '-', p.day), '%Y-%m-%d'),
+	DATE_FORMAT(CONCAT(c.year, '-', c.month, '-', c.day), '%Y-%m-%d')) / 10) * 10 AS age_category, COUNT(DISTINCT p.id)
+FROM wca.Persons AS p USE INDEX()
+JOIN wca.Results AS r ON r.personId = p.id AND best > 0
+JOIN wca.Competitions AS c ON c.id = r.competitionId
+WHERE p.year > 0 AND p.year <= YEAR(CURDATE()) - 40
+AND p.subid = 1
+GROUP BY age_category
+ORDER BY age_category;
