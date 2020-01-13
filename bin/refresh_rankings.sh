2c5b416b7715a3f789738597421ea32e9e9f86d0
Logiqx <software@mikeg.me.uk>
2020-01-13 14:05:55 +0000

    Run recent competitions with rankings

--- bin/refresh_rankings.sh
+++ bin/refresh_rankings.sh
@@ -3,3 +3,6 @@
 
 # Refresh Rankings
 run_py_script Senior_Rankings.py
+
+# Refresh Competitions
+run_py_script Recent_Competitions.py
---
+++ sql/extract_senior_participants.sql
@@ -0,0 +1,15 @@
+/* 
+    Script:   Extract Senior Participants
+    Created:  2020-01-13
+    Author:   Michael George / 2015GEOR02
+   
+    Purpose:  Extract list of recent competitions with senior participation
+*/
+
+SELECT DISTINCT r.competitionId, r.personId
+FROM Seniors AS p
+JOIN wca.Results AS r ON r.personId = p.personId
+JOIN wca.Competitions AS c ON c.id = r.competitionId
+	AND TIMESTAMPDIFF(YEAR, dob, DATE_FORMAT(CONCAT(c.year, '-', c.month, '-', c.day), '%Y-%m-%d')) >= 40
+	AND TIMESTAMPDIFF(DAY, CURDATE(), DATE_FORMAT(CONCAT(c.year + IF(c.endMonth < c.month, 1, 0), '-', c.endMonth, '-', c.endDay), '%Y-%m-%d')) >= -91
+ORDER BY competitionId, personId;
