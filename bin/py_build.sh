4482108b7207e01826a0634a63c4c47ee04d66cf
Logiqx <software@mikeg.me.uk>
2021-12-29 14:14:02 +0000

    Add simple hack to force IPv4 - required on my Windows laptop for Alpine 3.13 (and newer)

---
+++ bin/py_build.sh
@@ -0,0 +1,15 @@
+# Project Env
+. $(dirname $0)/env.sh
+
+# Determine Tag
+IMAGE_NAME=$PROJ_NAME
+IMAGE_TAG=tmp
+
+# Docker Build
+DOCKER_BUILDKIT=1 docker build . -f Dockerfile -t $IMAGE_NAME:$IMAGE_TAG
+
+# Copy the library scripts
+docker run --rm --entrypoint cat $IMAGE_NAME:$IMAGE_TAG $WORK_DIR/python/Force_IPv4.py >$PROJ_DIR/python/Force_IPv4.py
+
+# Clear pycache
+rm -f $PROJ_DIR/python/__pycache__/*.*.pyc
