70536f90babf1a3105fefbec3a81ecffca2266e1
Logiqx <software@mikeg.me.uk>
2022-01-28 12:31:14 +0000

    Allow persons to be listed in future + recent competitions even if they don;t have senior results

--- sql/extract_seniors.sql
+++ sql/extract_seniors.sql
@@ -10,16 +10,10 @@ UPDATE Seniors AS s
 JOIN wca.Persons AS p ON p.id = s.personId AND p.subid = 1
 SET s.name = p.name, s.countryId = p.countryId, s.gender = p.gender;
 
-SELECT t.personId, s.name AS personName, c.iso2 AS country, s.username, s.usernum, MAX(t.ageCategory) AS ageCategory, s.hidden, IFNULL(s.userId, 0) AS userId, s.deceased
-FROM
-(
-    SELECT s.personId, FLOOR(TIMESTAMPDIFF(YEAR, dob, STR_TO_DATE(CONCAT(c.year, '-', c.month, '-', c.day), '%Y-%m-%d')) / 10) * 10 AS ageCategory
-    FROM Seniors AS s
-    JOIN wca.Results AS r ON r.personId = s.personId
-    JOIN wca.Competitions AS c ON c.id = r.competitionId
-    HAVING ageCategory >= 40
-) AS t
-INNER JOIN Seniors AS s ON s.personId = t.personId
+SELECT s.personId, s.name AS personName, c.iso2 AS country, s.username, s.usernum,
+		FLOOR(TIMESTAMPDIFF(YEAR, dob, NOW()) / 10) * 10 AS ageCategory,
+		s.hidden, IFNULL(s.userId, 0) AS userId, s.deceased
+FROM Seniors AS s
 INNER JOIN wca.Countries AS c ON c.id = s.countryId
-GROUP BY personId
+HAVING ageCategory >= 40
 ORDER BY personId;
